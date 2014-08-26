03ec7a3c453f6a62f64f4ebfbee3d390d7a67497
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-08-26 10:12:33 +0000

    Fixed indigo setup
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@26648 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- setup-indigo.bash
+++ setup-indigo.bash
@@ -6,6 +6,8 @@ if [ -f ~/ros/$ROS_DISTRO/catkin_ws/devel/setup.bash ]
 then
     source ~/ros/$ROS_DISTRO/catkin_ws/devel/setup.bash
 else
+    sudo apt-get install g++
+    mkdir -p ~/ros/$ROS_DISTRO/catkin_ws/src
     cd ~/ros/$ROS_DISTRO/catkin_ws
     catkin_make
     source ~/ros/$ROS_DISTRO/catkin_ws/devel/setup.bash
