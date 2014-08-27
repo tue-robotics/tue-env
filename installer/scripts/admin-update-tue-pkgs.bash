e32c2b21b36e0440686332a26918be238a5ff8bd
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-08-27 16:28:54 +0000

    Updated admin-update-tue-pkgs
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@26747 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- installer/scripts/admin-update-tue-pkgs.bash
+++ installer/scripts/admin-update-tue-pkgs.bash
@@ -8,34 +8,6 @@ function svn-info {
 
 targets_dir=~/.tue/installer/targets
 
-# Create tue-all target
-mkdir -p $targets_dir/tue-all
-echo '' > $targets_dir/tue-all/install
-
-# Update ROSBUILD packages
-a=`find ~/svn/tue/trunk-rosbuild -name manifest.xml`
-for path in $a
-do
-    # Get rid of /manifest.xml
-    pkg_path=${path%/manifest.xml}
-
-    # Remove everything before the last slash
-    pkg_name=${pkg_path##*/};
-
-    echo "mkdir -p $targets_dir/${pkg_name}-ros"
-
-    url=`svn-info $pkg_path URL`
-
-    mkdir -p $targets_dir/${pkg_name}-ros
-
-    echo $url
-
-    echo "tue-install-ros-rosbuild svn $url" > $targets_dir/${pkg_name}-ros/install
-
-    # Add target to tue-all
-    echo "tue-install-target ${pkg_name}-ros" >> $targets_dir/tue-all/install
-done
-
 # Update CATKIN packages
 a=`find ~/svn/tue/trunk-catkin -name package.xml`
 for path in $a
@@ -46,17 +18,16 @@ do
     # Remove everything before the last slash
     pkg_name=${pkg_path##*/};
 
-    echo "mkdir -p $targets_dir/${pkg_name}-ros"
-
     url=`svn-info $pkg_path URL`
 
-    mkdir -p $targets_dir/${pkg_name}-ros
+    mkdir -p $targets_dir/ros-${pkg_name}
 
     echo $url
 
-    echo "tue-install-ros svn $url" > $targets_dir/${pkg_name}-ros/install
-
-    # Add target to tue-all
-    echo "tue-install-target ${pkg_name}-ros" >> $targets_dir/tue-all/install
+    echo """- type: ros
+  source:
+      type: svn
+      url: $url
+""" > $targets_dir/ros-${pkg_name}/install.yaml
 done
 
