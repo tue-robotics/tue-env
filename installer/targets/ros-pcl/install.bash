69f441e021705071b00edbbddd52ab6853efa303
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-08-27 15:44:11 +0000

    Added install bash
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@26741 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ installer/targets/ros-pcl/install.bash
@@ -0,0 +1,4 @@
+if [ "$ROS_DISTRO" == "groovy" ]
+then
+    tue-install-ros system pcl
+fi
---
+++ installer/targets/ros/install.bash
@@ -0,0 +1,43 @@
+if [ -z "$TUE_ROS_DISTRO" ]
+then
+    TUE_ROS_DISTRO=hydro
+fi
+
+if [ ! -d /opt/ros/$TUE_ROS_DISTRO ]
+then
+    sudo sh -c 'echo "deb http://ros.informatik.uni-freiburg.de/packages/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
+
+    wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+
+    sudo apt-get update
+
+    # Install basic ROS packages. All other packages will be installed using tue-rosdep
+    sudo apt-get install -y ros-$TUE_ROS_DISTRO-ros-base cmake python-catkin-pkg python-empy python-nose python-setuptools libgtest-dev build-essential
+
+    sudo rosdep init || true # make sure it always succeeds, even if rosdep init was already called
+
+    rosdep update
+fi
+
+TUE_SYSTEM_DIR=~/ros/$TUE_ROS_DISTRO/system
+
+if [ ! -f $TUE_SYSTEM_DIR/devel/setup.bash ]
+then
+    mkdir -p $TUE_SYSTEM_DIR/src
+    sudo apt-get install g++
+    cd $TUE_SYSTEM_DIR
+    catkin_make
+    source $TUE_SYSTEM_DIR/devel/setup.bash
+fi
+
+TUE_DEV_DIR=~/ros/$TUE_ROS_DISTRO/dev
+
+if [ ! -f $TUE_DEV_DIR/devel/setup.bash ]
+then
+    mkdir -p $TUE_DEV_DIR/src
+    sudo apt-get install g++
+    cd $TUE_DEV_DIR
+    catkin_make
+    source $TUE_DEV_DIR/devel/setup.bash
+fi
+
