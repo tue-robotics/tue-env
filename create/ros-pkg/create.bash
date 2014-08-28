57a05d9633c4e41defd63c2a0190690aba324e4b
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-08-28 12:58:43 +0000

    Better tue-create ros-pkg
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@26800 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- create/ros-pkg/create.bash
+++ create/ros-pkg/create.bash
@@ -2,7 +2,7 @@
 
 if [ -z "$1" ]
 then
-    echo "Usage: tue-create ros-pkg PACKAGE_NAME"
+    echo "Usage: tue-create ros-pkg PACKAGE_NAME [ DEPENDENCY1 DEPENDENCY2 ... ]"
     return 1
 fi
 
@@ -12,8 +12,111 @@ then
     return 1
 fi
 
-catkin_create_pkg $@
+deps=${@:2}
 
-#mkdir $1
-#cp ~/.tue/create/ros-pkg/package.xml $1
-#cp ~/.tue/create/ros-pkg/CMakeLists.txt $1
+mkdir $1
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# package.xml
+
+package_file=$1/package.xml
+
+echo '<?xml version="1.0"?>
+<package>
+  <name>ed_examples</name>
+  <version>0.0.0</version>
+  <description>No description available</description>
+
+  <maintainer email="todo@tue.nl">todo</maintainer>
+
+  <license>TODO</license>
+
+  <buildtool_depend>catkin</buildtool_depend>
+' > $package_file
+
+for dep in $deps
+do
+    echo "  <build_depend>$dep</build_depend>" >> $package_file
+    echo "  <run_depend>$dep</run_depend>" >> $package_file
+    echo "" >> $package_file
+    echo $dep
+done
+
+echo '</package>' >> $package_file
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# CMakeLists.txt
+
+cmake_file=$1/CMakeLists.txt
+
+echo "cmake_minimum_required(VERSION 2.8.3)
+project($1)
+" > $cmake_file
+
+echo 'find_package(catkin REQUIRED COMPONENTS' >> $cmake_file
+
+for dep in $deps
+do
+    echo "    $dep" >> $cmake_file
+done
+echo ')
+
+# find_package(Boost REQUIRED COMPONENTS system program_options)
+# find_package(PCL REQUIRED)
+# find_package(OpenCV REQUIRED)
+
+# ------------------------------------------------------------------------------------------------
+#                                          CATKIN EXPORT
+# ------------------------------------------------------------------------------------------------
+
+catkin_package(
+#  INCLUDE_DIRS include
+#  LIBRARIES bla
+#  CATKIN_DEPENDS other_catkin_pkg
+#  DEPENDS system_lib
+)
+
+# ------------------------------------------------------------------------------------------------
+#                                     ROS MESSAGES AND SERVICES
+# ------------------------------------------------------------------------------------------------
+
+# Generate messages
+# add_message_files(
+#    FILES
+#    message1.msg
+#    ...
+# )
+
+# Generate services
+# add_service_files(
+#    FILES
+#    service1.srv
+#    ...
+# )
+
+# Generate added messages and services with any dependencies listed here
+# generate_messages(
+#    DEPENDENCIES
+#    geometry_msgs
+#    ...
+# )
+
+# ------------------------------------------------------------------------------------------------
+#                                              BUILD
+# ------------------------------------------------------------------------------------------------
+
+add_libary(library_name
+    src/lib_source_file1.cpp
+    ...
+)
+
+add_executable(exec_name
+    src/source_file1.cpp
+    ...
+)
+
+' >> $cmake_file
+
+
+
