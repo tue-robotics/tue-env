f6e1c119c96642bed40b91e725c7022f35cf7501
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-09-01 11:40:48 +0000

    Also supports old way of installing
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@26910 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- setup.bash
+++ setup.bash
@@ -1,7 +1,13 @@
-source ~/.tue/setup/tue.bash_functions
-if [ -f ~/.tue/setup/target_setup.bash ]
+if [ -n "ROS_DISTRO_DEFAULT" ]
 then
-    source ~/.tue/setup/target_setup.bash
+    source ~/ros/groovy/catkin_ws/src/tue/trunk/amigo_admin_files/bash/amigo_user.bashrc
 else
-    tue-setup
+
+    source ~/.tue/setup/tue.bash_functions
+    if [ -f ~/.tue/setup/target_setup.bash ]
+    then
+        source ~/.tue/setup/target_setup.bash
+    else
+        tue-setup
+    fi
 fi
