23aee0b7b87819cb81be6ec862dfee4aef9fd0fc
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-09-03 09:38:56 +0000

    Added groovy installation for soem packages
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@26986 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ installer/targets/ros-soem_master/install.bash
@@ -0,0 +1,7 @@
+if [[ $TUE_ROS_DISTRO == "groovy" ]]
+then
+    tue-install-ros-rosbuild git http://git.mech.kuleuven.be/robotics/soem.git soem_master d9a96dd71885f6f147aa7522b3131974718a305a
+elif [[ $TUE_ROS_DISTRO == "hydro" ]]
+then
+    tue-install-ros git https://github.com/orocos/rtt_soem soem_master
+fi
