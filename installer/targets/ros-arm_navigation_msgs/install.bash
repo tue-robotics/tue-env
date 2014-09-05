85a721b64b28960860313f2ea95965650eee0e16
Ramon Wijnands <rayman747@hotmail.com>
2014-09-05 14:40:37 +0000

    checkout the correct branch
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@27170 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- installer/targets/ros-arm_navigation_msgs/install.bash
+++ installer/targets/ros-arm_navigation_msgs/install.bash
@@ -2,5 +2,5 @@ if [[ $TUE_ROS_DISTRO == "groovy" ]]
 then
     tue-install-ros system arm-navigation
 else
-    tue-install-ros git https://github.com/PR2/arm_navigation_msgs.git
+    tue-install-ros git https://github.com/PR2/arm_navigation_msgs.git . hydro-devel
 fi
