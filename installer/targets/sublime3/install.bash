cee8b726fc70af2f78971a526a23af03b55f74ef
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-09-15 12:38:07 +0000

    Automatically installs sublime package control
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@27389 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- installer/targets/sublime3/install.bash
+++ installer/targets/sublime3/install.bash
@@ -2,6 +2,7 @@ if [ ! -f /etc/apt/sources.list.d/webupd8team-sublime-text-3-precise.list ]
 then
     sudo add-apt-repository ppa:webupd8team/sublime-text-3
     sudo apt-get update
+	~/.tue/installer/targets/sublime3/sublime-package-control.py
 fi
 
 tue-install-system sublime-text-installer
