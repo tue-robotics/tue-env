38f568543930b65aeb13adb01b6800714a8cffb5
Ramon Wijnands <rayman747@hotmail.com>
2014-09-15 10:54:45 +0000

    add chrome
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@27378 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ installer/targets/chrome/install.bash
@@ -0,0 +1,9 @@
+if [ ! -f /etc/apt/sources.list.d/google-chrome.list ]
+then
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+    sudo apt-get update
+fi
+
+# install Gazebo
+tue-install-system google-chrome-stable
