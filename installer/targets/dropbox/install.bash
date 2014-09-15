5ce74047d3a77767e8ab3e097cd04391af964948
Ramon Wijnands <rayman747@hotmail.com>
2014-09-15 11:05:22 +0000

    add dropbox
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@27383 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ installer/targets/dropbox/install.bash
@@ -0,0 +1,8 @@
+if [ ! -f /etc/apt/sources.list.d/dropbox.list ]
+then
+    sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+    sudo sh -c 'echo "deb http://linux.dropbox.com/ubuntu/ trusty main" >> /etc/apt/sources.list.d/dropbox.list'
+    sudo apt-get update
+fi
+
+tue-install-system dropbox
