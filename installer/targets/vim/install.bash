fc9d8afa5702969693dc28bd719b47a8c4a513b3
Ramon Wijnands <rayman747@hotmail.com>
2014-09-15 12:22:45 +0000

    add vim
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@27386 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ installer/targets/vim/install.bash
@@ -0,0 +1,30 @@
+#/bin/bash
+
+# a basic vimrc
+
+if [ ! -f ~/.vimrc ]
+then
+	echo "installing a basic vimrc..."
+    # install a TU/e .vimrc
+	cp ~/.tue/installer/targets/vim/vimrc ~/.vimrc
+fi
+
+# install pathogen for automatic vim plugin loading
+if [ ! -d ~/.vim/autoload ]
+then
+    # install pathogen.vim
+    # https://github.com/tpope/vim-pathogen
+    mkdir -p ~/.vim/autoload ~/.vim/bundle
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+    # Now you can install any plugin into a .vim/bundle/plugin-name/ folder
+fi
+
+if [ ! -d ~/vim/bundle/editorconfig-vim ]
+then
+    echo "installing editorconfig..."
+    git clone https://github.com/editorconfig/editorconfig-vim.git ~/vim/bundle/editorconfig-vim
+fi
+
+tue-install-system vim
+# sudo update-alternatives --set editor /usr/bin/vim.basic
