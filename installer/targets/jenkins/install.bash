6cb6c5837373fd90a363e10eeedc9487dd8776ba
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-09-16 20:09:38 +0000

    Added install target for jenkins
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@27439 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ installer/targets/jenkins/install.bash
@@ -0,0 +1,9 @@
+if [ -f ! /etc/apt/sources.list.d/jenkins.list ]
+then
+		
+	wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+	sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+	sudo apt-get update
+fi
+
+tue-install-system jenkins
