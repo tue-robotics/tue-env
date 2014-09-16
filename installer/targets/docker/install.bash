6eff4f554b804e687011428c89eb0963a83ba648
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-09-16 20:05:12 +0000

    Update docker install
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@27438 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- installer/targets/docker/install.bash
+++ installer/targets/docker/install.bash
@@ -1,4 +1,17 @@
 if [ ! -f /etc/apt/sources.list.d/docker.list ]
 then
 	wget https://get.docker.io/ubuntu/ -O /tmp/docker-install && sudo sh /tmp/docker-install
+
+	# Add the docker group if it doesn't already exist.
+	sudo groupadd docker
+
+	# Add the connected user "${USER}" to the docker group.
+	# Change the user name to match your preferred user.
+	# You may have to logout and log back in again for
+	# this to take effect.
+	sudo gpasswd -a ${USER} docker
+
+	# Restart the Docker daemon.
+	# If you are in Ubuntu 14.04, use docker.io instead of docker
+	sudo service docker restart
 fi
