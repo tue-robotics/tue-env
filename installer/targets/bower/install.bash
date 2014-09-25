de468a5ba5b51ad715da32f783093afccce27e33
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-09-25 12:43:10 +0000

    Fixed bower and grunt targets
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@27572 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- installer/targets/bower/install.bash
+++ installer/targets/bower/install.bash
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-if [ ! hash -q bower &> /dev/null ]
+if ! hash bower &> /dev/null
 then
+    # We need the nodejs package manager
+    tue-install-target nodejs
+
+    # Unfortunately this is necessary. tue-get only installs
+    # system debs in the end (TODO: make nicer)
+    sudo apt-get install nodejs
+
 	sudo -H npm install -g bower
 fi
 
