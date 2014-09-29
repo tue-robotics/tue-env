35618c66de99b6e0ca906906f505a4e0dc0d4c15
Max Baeten <m.j.j.baeten@student.tue.nl>
2014-09-29 11:41:38 +0000

    removed obs soem_core for hydro
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@27600 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- installer/targets/ros-soem_core/install.bash
+++ installer/targets/ros-soem_core/install.bash
@@ -2,7 +2,4 @@ if [[ $TUE_ROS_DISTRO == "groovy" ]]
 then
     tue-install-ros-rosbuild git http://git.mech.kuleuven.be/robotics/soem.git soem_core d9a96dd71885f6f147aa7522b3131974718a305a
     tue-install-apply-patch patch_groovy.patch
-elif [[ $TUE_ROS_DISTRO == "hydro" ]]
-then
-    tue-install-ros git https://github.com/orocos/rtt_soem soem_core
 fi
