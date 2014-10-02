fcf02b32ba507723fed5390a1c676a6b639a7e17
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-10-02 14:36:45 +0000

    Fixed installer for philips_text_to_speech
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@27675 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- installer/targets/ros-philips_text_to_speech/install.bash
+++ installer/targets/ros-philips_text_to_speech/install.bash
@@ -1,1 +1,15 @@
-tue-install-ros svn https://roboticssrv.wtb.tue.nl/svn/3rdparty/philips_text_to_speech
+if [ ! -d ~/ros/hydro/system/src/philips_text_to_speech ]
+then
+   URL=https://roboticssrv.wtb.tue.nl/svn/3rdparty/philips_text_to_speech
+   DEST="$HOME/ros/$ROS_DISTRO/repos/https://roboticssrv.wtb.tue.nl/svn/3rdparty/philips_text_to_speech"
+   LINK=$TUE_SYSTEM_DIR/src/philips_text_to_speech
+
+    tue-install-warning """Cannot install 'philips_text_to_speech' automatically because it is password protected. Please run manually:
+
+    svn co $URL \\
+           '$DEST' && \\
+    ln -s  '$DEST' \\
+           $LINK
+"""
+
+fi
