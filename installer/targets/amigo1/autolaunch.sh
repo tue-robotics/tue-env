cd6d70f791bc5555088d936e9c31f171a3a189ed
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-10-02 11:03:36 +0000

    Added autolaunch script
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@27646 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ installer/targets/amigo1/autolaunch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check argument
+if [ -z "$1" ]
+  then
+    echo "No pid supplied"
+elif [ -z "$2" ]
+  then
+    echo "No package supplied"
+elif [ -z "$3" ]
+  then
+    echo "No launchfile supplied"
+fi
+
+
+# Create dummy graphic environment
+PS1=rommel
+
+# Source ros stuff as user is accustomed to
+source /home/amigo/.bashrc
+
+# Launch the file
+roslaunch --pid $1 --wait $2 $3
+
