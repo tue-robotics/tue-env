6584caa26231254b2e720fca4f5cd11ae692abb1
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-10-20 07:37:44 +0000

    Added target for zeromq
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@27945 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ installer/targets/zeromq/install.bash
@@ -0,0 +1,10 @@
+if [ ! -f /usr/local/lib/zeromq.so.4 ]
+then
+	wget http://download.zeromq.org/zeromq-4.1.0-rc1.tar.gz -O /tmp/zeromq-4.1.0-rc1.tar.gz
+	tar -zxvf /tmp/zeromq-4.1.0-rc1.tar.gz -C /tmp
+	cd /tmp/zeromq-4.1.0
+	./configure
+	make
+	sudo make install
+	cd -
+fi
