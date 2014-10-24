37863c88132d0b9f59bd3a870a891a301ea93ed6
Ramon Wijnands <rayman747@hotmail.com>
2014-10-24 11:35:09 +0000

    install git-extras and enable color
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@28044 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- installer/targets/git/install.bash
+++ installer/targets/git/install.bash
@@ -3,7 +3,10 @@
 # check for git-extras
 if [ ! -f /usr/local/bin/git-extras ]; then
 	# https://github.com/tj/git-extras
-	#(cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && sudo make install)
+	cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && sudo make install
+
+	# enable color
+	git config --global color.ui true
 fi
 
 tue-install-system git gitk git-gui
