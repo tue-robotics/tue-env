2f71ed2de85eb095e959ee30020d8de87d84576c
Ramon Wijnands <rayman747@hotmail.com>
2014-10-28 13:31:05 +0000

    use wget
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@28108 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- installer/targets/spot/install.bash
+++ installer/targets/spot/install.bash
@@ -2,5 +2,5 @@
 
 if [ ! -f /usr/local/bin/spot ]
 then
-	sudo bash -c "curl https://raw.githubusercontent.com/guille/spot/master/spot.sh > /usr/local/bin/spot ; chmod +x /usr/local/bin/spot"
+	sudo bash -c "wget https://raw.githubusercontent.com/guille/spot/master/spot.sh -O /usr/local/bin/spot ; chmod +x /usr/local/bin/spot"
 fi
