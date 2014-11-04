decaea8abd6533dd3a1224e00b9e9306f98800f5
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-11-04 19:40:44 +0000

    Updated
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@28191 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- installer/targets/ros-control_toolbox/install.bash
+++ installer/targets/ros-control_toolbox/install.bash
@@ -3,5 +3,5 @@ then
     tue-install-ros svn https://roboticssrv.wtb.tue.nl/svn/data/mirror/wg-ros-pkg/control_toolbox .
 elif [[ $TUE_ROS_DISTRO == "hydro" ]]
 then
-    tue-install-ros system pr2-controllers
+    tue-install-ros system control-toolbox
 fi
