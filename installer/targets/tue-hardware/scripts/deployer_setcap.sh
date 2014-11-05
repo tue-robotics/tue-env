dee743e88571256cabe9e03260bb003b9cee03c2
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-11-05 16:45:21 +0000

    Added deployer_setcap script
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/tue_env@28210 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ installer/targets/tue-hardware/scripts/deployer_setcap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "======================"
+echo "SETCAP SCRIPT - KOEN BUYS"
+# Modified by Tim Clephas
+echo "======================"
+echo "DANGER: this script is created to reset the capabilities of the deployer-gnulinux in OCL "
+
+deployer=$(rospack find ocl)/bin/deployer-gnulinux
+cdeployer=$(rospack find ocl)/bin/cdeployer-gnulinux
+corbadeployer=$(rospack find ocl)/bin/deployer-corba-gnulinux
+luadeployer=$(rospack find ocl)/bin/rttlua-gnulinux
+
+if [ ! -f $deployer ]
+then
+    echo "Deployer does not exist"
+    exit
+fi
+
+sudo apt-get install libcap2-bin -y
+
+sudo setcap cap_net_raw+ep $deployer cap_net_raw+ep $cdeployer cap_net_raw+ep $corbadeployer cap_net_raw+ep $luadeployer
+
+echo "should be finished now : don't risk to do this on other files!"
