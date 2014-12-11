820f1d67059ac48f2a305b581af6a60a86aae659
Ramon Wijnands <rayman747@hotmail.com>
2014-12-11 12:33:53 +0100

    fix yaml-cpp installation under 12.04

--- installer/targets/ros-yaml-cpp/install.bash
+++ installer/targets/ros-yaml-cpp/install.bash
@@ -2,7 +2,7 @@ ubuntu_version=$(lsb_release -sr)
 
 if [ $ubuntu_version == 12.04 ]
 then
-    tue-install-system libyaml-dev
+    tue-install-system yaml-cpp
 else
     tue-install-system libyaml-cpp-dev
 fi
