209ae96ca95dff7057a3c359908eb2327a5f8c72
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2015-01-31 15:07:20 +0100

    Made some targets work on indigo

--- installer/targets/ros-soem_beckhoff_drivers/install.bash
+++ installer/targets/ros-soem_beckhoff_drivers/install.bash
@@ -2,7 +2,6 @@ if [[ $TUE_ROS_DISTRO == "groovy" ]]
 then
     tue-install-ros-rosbuild git http://git.mech.kuleuven.be/robotics/soem.git soem_beckhoff_drivers d9a96dd71885f6f147aa7522b3131974718a305a
     tue-install-apply-patch patch_groovy.patch
-elif [[ $TUE_ROS_DISTRO == "hydro" ]]
-then
+else
     tue-install-ros git https://github.com/orocos/rtt_soem soem_beckhoff_drivers
 fi
