f0fda12227e7c88177cd67e1b6e2f706ad991c5b
Ramon Wijnands <rayman747@hotmail.com>
2015-02-03 10:54:03 +0100

    fix sublime for trusty

--- installer/targets/sublime3/install.bash
+++ installer/targets/sublime3/install.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ ! -f /etc/apt/sources.list.d/webupd8team-sublime-text-3-precise.list ]
+source /etc/lsb-release
+
+if [ ! -f /etc/apt/sources.list.d/webupd8team-sublime-text-3-$DISTRIB_CODENAME.list ]
 then
     sudo add-apt-repository ppa:webupd8team/sublime-text-3
     sudo apt-get update
