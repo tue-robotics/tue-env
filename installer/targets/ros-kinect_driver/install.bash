3cdbee977f6216eae42414d034b175ad7fcdd4cb
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-02-26 15:48:43 +0100

    Only install libfreenect if needed

--- installer/targets/ros-kinect_driver/install.bash
+++ installer/targets/ros-kinect_driver/install.bash
@@ -1,4 +1,7 @@
-sudo add-apt-repository ppa:floe/libtisch
-sudo apt-get update
-sudo apt-get install libfreenect libfreenect-dev libfreenect-demos
+if [ ! -d /usr/local/include/libfreenect ]
+then
+    sudo add-apt-repository ppa:floe/libtisch
+    sudo apt-get update
+    sudo apt-get install libfreenect libfreenect-dev libfreenect-demos
+fi
 
