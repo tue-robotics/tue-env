761c40723740a84945590bc20ad9996419434961
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-04-14 15:38:33 +0200

    Fixed install of kinect_driver for ubuntu 14.04

--- installer/targets/ros-kinect_driver/install.bash
+++ installer/targets/ros-kinect_driver/install.bash
@@ -1,7 +1,12 @@
 if [ ! -d /usr/local/include/libfreenect ] && [ ! -f /usr/include/libfreenect.h ]
 then
-    sudo add-apt-repository ppa:floe/libtisch
-    sudo apt-get update
-    sudo apt-get install libfreenect libfreenect-dev libfreenect-demos
+    if [ `lsb_release -rs` == "12.04" ]
+    then
+        sudo add-apt-repository ppa:floe/libtisch
+        sudo apt-get update
+        sudo apt-get install libfreenect libfreenect-dev libfreenect-demos
+    else
+        sudo apt-get install libfreenect-dev
+    fi
 fi
 
