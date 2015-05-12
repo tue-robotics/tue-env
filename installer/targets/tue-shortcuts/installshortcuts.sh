10929f9c268e2f1dba2569dd1f52aa88d26a18e1
Max Baeten <m.j.j.baeten@student.tue.nl>
2015-05-12 16:48:04 +0200

    added shortcuts

---
+++ installer/targets/tue-shortcuts/installshortcuts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# copying shortcuts to desktop
+echo copying shortcuts to desktop, please drag them into the top bar
+cp ~/.tue/installer/targets/tue-shortcuts/shortcuts/*.desktop ~/Desktop
+
+# copying terminator config file
+read -r -p "Are you sure you want to replace .config/terminator/config? [y/N] " response
+response=${response,,}    # tolower
+if [[ $response =~ ^(yes|y)$ ]]
+then
+	cp ~/.config/terminator/config ~/.config/terminator/configbackup
+    cp ~/.tue/installer/targets/tue-shortcuts/terminatorconfig/config ~/.config/terminator/config
+    echo the config has been copied. Backup of old config: ~/.config/terminator/configbackup
+else
+	echo the config has not been copied, Therefore the working of the shortcuts cannot be guaranteed
+fi
