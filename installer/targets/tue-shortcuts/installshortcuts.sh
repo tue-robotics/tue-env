95334402e634f255e15f9c2d7a7cdb72bf46a5f0
Max Baeten <m.j.j.baeten@gmail.com>
2015-05-16 12:32:22 +0200

    finally done

--- installer/targets/tue-shortcuts/installshortcuts.sh
+++ installer/targets/tue-shortcuts/installshortcuts.sh
@@ -2,13 +2,13 @@
 
 # copying shortcuts to desktop
 echo copying shortcuts to desktop, please drag them into the top bar
+echo to do this, open a nautilus, go to the deskop, show hidden files and drag the hidden files to the top bar, do not delete the hidden files afterwards
 cp ~/.tue/installer/targets/tue-shortcuts/shortcuts/.*.desktop ~/Desktop
 
 # copying shortcuts to desktop
 echo copying icons to /usr/share/pixmaps/tue
 sudo mkdir /usr/share/pixmaps/tue
 sudo cp ~/.tue/installer/targets/tue-shortcuts/icons/*.png /usr/share/pixmaps/tue/
-
 
 # copying terminator config file
 read -r -p "Are you sure you want to replace .config/terminator/config? [y/N] " response
