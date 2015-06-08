11cd80015d567f21bd06e5de82204e9bde35d4a8
amigo <amigo@cst73>
2015-06-08 19:26:07 +0200

    fixed ruby install issue, ruby-gems is now called ruby

--- installer/targets/gitsu/install.bash
+++ installer/targets/gitsu/install.bash
@@ -1,5 +1,5 @@
 # gitsu is installed as a ruby gem
-hash gem 2> /dev/null || sudo apt-get install rubygems
+hash gem 2> /dev/null || sudo apt-get install ruby
 hash git-su 2> /dev/null || sudo gem install gitsu
 
 # install the authors file
