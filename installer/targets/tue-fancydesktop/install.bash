3c04a4a77b7b9c99ba9524436fcebc75cdd975b2
Max Baeten <m.j.j.baeten@student.tue.nl>
2015-06-08 19:27:33 +0200

    renamed tue-shortcuts target to tue-fancydesktop and fixed some stuff

---
+++ installer/targets/tue-fancydesktop/install.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+source /etc/lsb-release
+
+if ! ls ~/ros/$ROS_DISTRO/.env/installed | grep "tue-fancydesktop"
+then
+	# copying shortcuts to desktop
+	cp ~/.tue/installer/targets/tue-fancydesktop/shortcuts/.*.desktop ~/Desktop
+
+	# copying shortcuts to desktop
+	sudo mkdir -p /usr/share/pixmaps/tue
+	sudo cp ~/.tue/installer/targets/tue-fancydesktop/icons/*.png /usr/share/pixmaps/tue/
+
+	# copying terminator config file
+	read -r -p "Are you sure you want to replace .config/terminator/config? [y/N] " response
+	response=${response,,}    # tolower
+	if [[ $response =~ ^(yes|y)$ ]]
+	then
+		cp ~/.config/terminator/config ~/.config/terminator/configbackup
+		cp ~/.tue/installer/targets/tue-fancydesktop/configs/terminator/config ~/.config/terminator/config
+	else
+		echo the terminator config has not been copied, Therefore the working of the shortcuts cannot be guaranteed
+	fi
+
+	# install variety
+	read -r -p "Do you want to install variety to show fancy amigo pictures on your desktop? [y/N] " response
+	response=${response,,}    # tolower
+	if [[ $response =~ ^(yes|y)$ ]]
+	then
+		# First install variety
+		if ! grep -q -r --include \*.list "peterlevi" /etc/apt 
+		then
+			sudo add-apt-repository ppa:peterlevi/ppa
+			sudo apt-get update
+		fi
+		tue-install-system variety
+		
+		# Secondly fix variety config file
+		read -r -p "Are you sure you want to replace .config/variety/config? [y/N] " response
+		response=${response,,}    # tolower
+		if [[ $response =~ ^(yes|y)$ ]]
+		then
+			cp ~/.config/variety/variety.conf ~/.config/variety/variety.conf.backup
+			cp ~/.tue/installer/targets/tue-fancydesktop/configs/variety/variety.conf ~/.config/variety/variety.conf
+		else
+			echo the variety config has not been copied, Therefore the working of the shortcuts cannot be guaranteed
+		fi
+	fi
+
+	echo 
+	echo Finished. Please drag the hidden desktopicons from the desktop into the top bar
+fi
