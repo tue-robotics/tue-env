f2cec689ea60edaf9f79989010ce4d554a389a27
Ramon Wijnands <rayman747@hotmail.com>
2015-07-07 15:32:27 +0200

    fix spotify key

--- installer/targets/spotify/install.bash
+++ installer/targets/spotify/install.bash
@@ -2,7 +2,7 @@
 
 if [ ! -f /etc/apt/sources.list.d/spotify.list ]
 then
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2C19886
     sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list.d/spotify.list'
 
     sudo apt-get update
