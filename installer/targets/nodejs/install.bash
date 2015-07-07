fe471c06aad731ffbd85571b1873a9e750b11a82
Ramon Wijnands <rayman747@hotmail.com>
2015-07-07 19:19:22 +0200

    fix nodejs install target

--- installer/targets/nodejs/install.bash
+++ installer/targets/nodejs/install.bash
@@ -2,7 +2,11 @@
 
 if [ ! -f /etc/apt/sources.list.d/nodesource.list ]
 then
-	wget -qO- https://deb.nodesource.com/setup | bash -
+    # increase the amount of file watchers
+    echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+	wget -qO- https://deb.nodesource.com/setup | sudo bash -
+    sudo apt-get install nodejs
 fi
 
 tue-install-system nodejs
