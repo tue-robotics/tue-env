9711fa8a2f649b07048b48c7a25103a5e291a72d
Rokus <rokus@CST110.wtb.tue.nl>
2015-08-03 10:13:59 +0200

    No longer asking for sudo password when trying to install pillow

--- installer/targets/ros-python-pillow/install.bash
+++ installer/targets/ros-python-pillow/install.bash
@@ -1,7 +1,7 @@
 # Make sure python-pip is installed
 dpkg -s python-pip &> /dev/null || sudo apt-get install python-pip
 
-if [ ! $(pip freeze | grep pillow) ]
+if [ ! $(pip freeze | grep Pillow) ]
 then
 	sudo -H pip install -U pillow
 fi
