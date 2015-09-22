71905639e516b82cc3e306e06093a67740e9b213
Loy van Beek <loy.vanbeek@gmail.com>
2015-09-22 22:13:27 +0200

    Add installer for wit_ros

---
+++ installer/targets/ros-wit_ros/install.bash
@@ -0,0 +1,7 @@
+# Make sure python-pip is installed
+dpkg -s python-pip &> /dev/null || sudo apt-get install python-pip
+
+if [ ! $(pip freeze | grep graphviz) ]
+then
+	sudo -H pip install -U requests
+fi
