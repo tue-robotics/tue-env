8a824f7dfdcc7c319f195c2001053835abe47cd5
Loy van Beek <loy.vanbeek@gmail.com>
2015-12-01 20:17:47 +0100

    Installing orocos_kdl for robot_smach_states

--- installer/targets/ros-robot_smach_states/install.bash
+++ installer/targets/ros-robot_smach_states/install.bash
@@ -1,6 +1,8 @@
 # Make sure python-pip is installed
 dpkg -s python-pip &> /dev/null || sudo apt-get install python-pip
 
+sudo apt-get install -y ros-$TUE_ROS_DISTRO-orocos-kdl
+
 if [ ! $(pip freeze | grep graphviz) ]
 then
 	sudo -H pip install -U graphviz
