699dfc99226a5dbd597df28b67a41201827e6c34
Ramon Wijnands <rayman747@hotmail.com>
2015-12-15 15:54:23 +0100

    feat(target): also install dashboard-alpha releases

--- installer/targets/ros-dashboard/install.bash
+++ installer/targets/ros-dashboard/install.bash
@@ -1,23 +1,8 @@
 #!/bin/bash
+set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-RELEASES=`$DIR/get-releases linux64`
-LATEST=`echo $RELEASES | head -n 1`
-TAG=`echo $LATEST | awk '{ print $1 }'`
-URL=`echo $LATEST | awk '{ print $2 }'`
-
-#echo -e "found these versions:\n\n$RELEASES\n"
-#echo "latest: $TAG"
-
-if [ -f $TUE_BIN/dashboard/$TAG ]
-then
-    echo "dashboard: no updates found"
-else	
-    mkdir -p $TUE_BIN/dashboard
-    wget $URL -O- | tar -xzv --directory $TUE_BIN/dashboard
-	ln -s $TUE_BIN/dashboard/dashboard $TUE_BIN/tue-dashboard
-    touch $TUE_BIN/dashboard/$TAG
-fi
+$DIR/get-releases linux64 | ./process-releases
 
 VER=$(uname -r)
 if [ $VER = "14.04" ]
