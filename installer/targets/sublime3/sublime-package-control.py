d195126260648b238b68c9381040b9d518e1fe27
Ramon Wijnands <rayman747@hotmail.com>
2016-03-09 11:40:24 +0100

    fix(sublime): update package control download script

--- installer/targets/sublime3/sublime-package-control.py
+++ installer/targets/sublime3/sublime-package-control.py
@@ -1,19 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+import urllib.request
+import os
+import hashlib
 
-import urllib.request, os, hashlib;
-
-h = 'eb2297e1a458f27d836c04bb0cbaf282d0e7a3098092775ccb37ca9d6b2e4b7d';
-pf = 'Package Control.sublime-package';
+h = '2915d1851351e5ee549c20394736b4428bc59f460fa1548d1514676163dafc88'
+pf = 'Package Control.sublime-package'
 ipp = os.path.expanduser('~/.config/sublime-text-3/Installed Packages')
 
-if os.path.exists(ipp):
-	urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) );
-	by = urllib.request.urlopen( 'http://sublime.wbond.net/' + pf.replace(' ', '%20')).read();
-	dh = hashlib.sha256(by).hexdigest();
-	if dh != h:
-		print('Error validating download (got %s instead of %s), please try manual install' % (dh, h))
-	else:
-		open(os.path.join( ipp, pf), 'wb' ).write(by)
-		print('Done!')
-else:
-	print('Error, sublime path not found')
+urllib.request.install_opener(
+    urllib.request.build_opener(urllib.request.ProxyHandler()))
+by = urllib.request.urlopen(
+    'http://packagecontrol.io/' + pf.replace(' ', '%20')).read()
+dh = hashlib.sha256(by).hexdigest()
+print('Error validating download (got %s instead of %s), please try manual install' %
+      (dh, h)) if dh != h else open(os.path.join(ipp, pf), 'wb').write(by)
