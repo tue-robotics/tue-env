160d0fefc3ae1e48595e24db35769e58071c1e32
Loy van Beek <loy.vanbeek@gmail.com>
2016-03-19 15:42:56 +0100

    installer for openface and dependencies

---
+++ installer/targets/openface/install.bash
@@ -0,0 +1,28 @@
+pushd . 
+mkdir ~/openface
+cd ~/openface
+git clone https://github.com/cmusatyalab/openface.git --recursive
+sudo pip install numpy pandas scipy scikit-learn scikit-image
+
+bash dlib.bash
+
+bash torch.bash
+
+cd ~/openface
+sudo python2 setup.py install
+
+models/get-models.sh 
+
+popd
+
+if [[ $(python -c "import dlib") -eq 1 ]]; then
+	echo "DLIB is not properly installed"
+fi
+
+if [[ $(python -c "import cv2") -eq 1 ]]; then
+	echo "opencv2 is not properly installed"
+fi
+
+if [[ $(python -c "import openface") -eq 1 ]]; then
+	echo "openface is not properly installed"
+fi
