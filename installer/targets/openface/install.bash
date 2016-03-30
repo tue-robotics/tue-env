9aa4e02d658b4340c173830bab78dc77f8642d13
Rein Appeldoorn <reinzor@gmail.com>
2016-03-30 09:13:36 +0200

    check if dir exists

--- installer/targets/openface/install.bash
+++ installer/targets/openface/install.bash
@@ -1,29 +1,31 @@
-pushd . 
-mkdir ~/openface
-cd ~/openface
-git clone https://github.com/cmusatyalab/openface.git  ~/openface --recursive
-sudo pip install numpy pandas scipy scikit-learn scikit-image
+if [ ! -d ~/openface ]; then
+    pushd .
+    mkdir ~/openface
+    cd ~/openface
+    git clone https://github.com/cmusatyalab/openface.git  ~/openface --recursive
+    sudo pip install numpy pandas scipy scikit-learn scikit-image
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-bash $DIR/dlib.bash
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    bash $DIR/dlib.bash
 
-bash $DIR/torch.bash
+    bash $DIR/torch.bash
 
-cd ~/openface
-sudo python2 setup.py install
+    cd ~/openface
+    sudo python2 setup.py install
 
-models/get-models.sh 
+    models/get-models.sh
 
-popd
+    popd
 
-if [[ $(python -c "import dlib") -eq 1 ]]; then
-	echo "DLIB is not properly installed"
-fi
+    if [[ $(python -c "import dlib") -eq 1 ]]; then
+        echo "DLIB is not properly installed"
+    fi
 
-if [[ $(python -c "import cv2") -eq 1 ]]; then
-	echo "opencv2 is not properly installed"
-fi
+    if [[ $(python -c "import cv2") -eq 1 ]]; then
+        echo "opencv2 is not properly installed"
+    fi
 
-if [[ $(python -c "import openface") -eq 1 ]]; then
-	echo "openface is not properly installed"
+    if [[ $(python -c "import openface") -eq 1 ]]; then
+        echo "openface is not properly installed"
+    fi
 fi
