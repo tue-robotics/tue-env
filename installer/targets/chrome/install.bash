cbca82ae12009bdc46a28f5e3e052be023fe2d16
Ramon Wijnands <rayman747@hotmail.com>
2016-03-30 15:40:47 +0200

    fix(chrome): fix the chrome ppa

--- installer/targets/chrome/install.bash
+++ installer/targets/chrome/install.bash
@@ -2,8 +2,8 @@
 
 if [ ! -f /etc/apt/sources.list.d/google-chrome.list ]
 then
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
     sudo apt-get update
 fi
 
