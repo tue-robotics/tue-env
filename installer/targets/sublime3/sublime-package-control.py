345e201390e7a1103f630952fc3704b11e5de4a6
Ramon Wijnands <rayman747@hotmail.com>
2016-11-12 10:28:45 +0100

    Add fallback for sublime package control

--- installer/targets/sublime3/sublime-package-control.py
+++ installer/targets/sublime3/sublime-package-control.py
@@ -7,10 +7,16 @@ h = '2915d1851351e5ee549c20394736b4428bc59f460fa1548d1514676163dafc88'
 pf = 'Package Control.sublime-package'
 ipp = os.path.expanduser('~/.config/sublime-text-3/Installed Packages')
 
+print('Installing Sublime Package Control')
+
 urllib.request.install_opener(
     urllib.request.build_opener(urllib.request.ProxyHandler()))
-by = urllib.request.urlopen(
-    'http://packagecontrol.io/' + pf.replace(' ', '%20')).read()
-dh = hashlib.sha256(by).hexdigest()
-print('Error validating download (got %s instead of %s), please try manual install' %
-      (dh, h)) if dh != h else open(os.path.join(ipp, pf), 'wb').write(by)
+try:
+    by = urllib.request.urlopen(
+        'http://packagecontrol.io/' + pf.replace(' ', '%20')).read()
+except urllib.error.URLError:
+    print('Error, download failed, please try manual install')
+else:
+    dh = hashlib.sha256(by).hexdigest()
+    print('Error validating download (got %s instead of %s), please try manual install' %
+        (dh, h)) if dh != h else open(os.path.join(ipp, pf), 'wb').write(by)
