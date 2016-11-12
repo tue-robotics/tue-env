aeba3a5ac832e4f4a63e77c845254aa140aacbc1
Ramon Wijnands <rayman747@hotmail.com>
2016-11-12 11:17:03 +0100

    Add xenial support for the sublime installer

--- installer/targets/sublime3/install.bash
+++ installer/targets/sublime3/install.bash
@@ -2,11 +2,28 @@
 
 source /etc/lsb-release
 
-if [ ! -f /etc/apt/sources.list.d/webupd8team-sublime-text-3-$DISTRIB_CODENAME.list ]
+if [[ $DISTRIB_CODENAME = trusty ]]
 then
-    sudo add-apt-repository ppa:webupd8team/sublime-text-3
-    sudo apt-get update
-	~/.tue/installer/targets/sublime3/sublime-package-control.py
+
+    if [[ ! -f /etc/apt/sources.list.d/webupd8team-sublime-text-3-$DISTRIB_CODENAME.list ]]
+    then
+        sudo add-apt-repository ppa:webupd8team/sublime-text-3
+        sudo apt-get update
+        ~/.tue/installer/targets/sublime3/sublime-package-control.py
+    fi
+
+elif [[ $DISTRIB_CODENAME = xenial ]]
+then
+
+    if [[ ! -f /etc/apt/sources.list.d/webupd8team-ubuntu-sublime-text-3-$DISTRIB_CODENAME.list ]]
+    then
+        sudo add-apt-repository ppa:webupd8team/sublime-text-3
+        sudo apt-get update
+        ~/.tue/installer/targets/sublime3/sublime-package-control.py
+    fi
+
+else
+    echo "Error: unknown distribution: $DISTRIB_CODENAME"
 fi
 
 tue-install-system sublime-text-installer
