36530a408edb9ccfadf057bbecb2de0368b6b151
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2016-11-29 22:21:53 +0100

    Updated tensorflow installer

--- installer/targets/ros-tensorflow_ros/install.bash
+++ installer/targets/ros-tensorflow_ros/install.bash
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 # Make sure python-pip is installed
-dpkg -s python-pip &> /dev/null || sudo apt-get install python-pip
+dpkg -s python-pip &> /dev/null || sudo apt-get install python-pip -y
 
 if [ ! $(pip freeze | grep tensorflow) ]
 then
     export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0rc0-cp27-none-linux_x86_64.whl
 
     # ToDo: use pip target
-    # ToDo: sudo not necessary for 16.04
-    echo -e "\nInstalling tensorflow using pip. If you're using Ubuntu 16.04, this bash script probably needs to be updated\n"
+    echo -e "\nInstalling tensorflow using pip.\n"
 
-    sudo pip install -U $TF_BINARY_URL
+    if [[ $(lsb_release -rs) > "16.03" ]] 
+    then
+        pip install -U $TF_BINARY_URL --user
+    else
+        sudo pip install -U $TF_BINARY_URL
+    fi
 else
     echo -e "Tensorflow already installed"
 fi
