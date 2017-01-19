515c27f5787bd55c94938aff7f1c2bc4d0a8c54e
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-01-19 14:14:06 +0100

    Easy targets changed, still a lot of cleanup to do

--- installer/targets/ros-python-xhtml2pdf/install.bash
+++
@@ -1,7 +0,0 @@
-# Make sure python-pip is installed
-dpkg -s python-pip &> /dev/null || sudo apt-get install python-pip
-
-if [ ! $(pip freeze | grep xhtml2pdf) ]
-then
-	sudo -H pip install -U xhtml2pdf
-fi
