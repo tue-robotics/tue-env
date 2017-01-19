695d17810bea3044fea2c3cc0c391f3b1dd1d19c
Loy <LoyVanBeek@users.noreply.github.com>
2017-01-19 14:30:40 +0100

    Fix #32

--- installer/targets/ros-wit_ros/install.bash
+++ installer/targets/ros-wit_ros/install.bash
@@ -1,7 +1,7 @@
 # Make sure python-pip is installed
 dpkg -s python-pip &> /dev/null || sudo apt-get install python-pip
 
-if [ ! $(pip freeze | grep graphviz) ]
+if [ ! $(pip freeze | grep requests) ]
 then
 	sudo -H pip install -U requests
 fi
