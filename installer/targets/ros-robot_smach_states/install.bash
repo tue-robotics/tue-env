41375b80e84184f05ea863eb53e91b0b3f192d69
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-01-22 14:22:15 +0100

    Getting rid of pip in bash for robot_smach_states

--- installer/targets/ros-robot_smach_states/install.bash
+++ installer/targets/ros-robot_smach_states/install.bash
@@ -2,13 +2,3 @@
 dpkg -s python-pip &> /dev/null || sudo apt-get install python-pip
 
 dpkg -s ros-$TUE_ROS_DISTRO-orocos-kdl &> /dev/null && dpkg -s ros-$TUE_ROS_DISTRO-python-orocos-kdl &> /dev/null || sudo apt-get install -y ros-$TUE_ROS_DISTRO-orocos-kdl ros-$TUE_ROS_DISTRO-python-orocos-kdl
-
-if [ ! $(pip freeze | grep graphviz) ]
-then
-	sudo -H pip install -U graphviz
-fi
-
-if [ ! $(pip freeze | grep psutil) ]
-then
-	sudo -H pip install -U psutil
-fi
