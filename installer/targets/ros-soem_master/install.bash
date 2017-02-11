88e92e7aaa22ceead037903b2644b82a79f4ec3c
Rein Appeldoorn <reinzor@gmail.com>
2017-02-11 14:53:03 +0100

    fix(): soem_master target & feature(): auto setup realtime priorities
    Soem master now installed via install.yaml and realtime priorities are
    set automatically

--- installer/targets/ros-soem_master/install.bash
+++ installer/targets/ros-soem_master/install.bash
@@ -1,3 +1,10 @@
-tue-install-ros git https://github.com/orocos/rtt_soem soem_master
-# Install orocos
-tue-install-target orocos
+SUDO_PAM_LIMITS="session    required   pam_limits.so"
+PAM_SUDO_FILE="/etc/pam.d/sudo"
+
+REALTIME_LIMITS="amigo            soft    rtprio          99\namigo            hard    rtprio          99"
+LIMITS_FILE="/etc/security/limits.conf"
+
+if ! grep -q "$SUDO_PAM_LIMITS" $PAM_SUDO_FILE ; then
+    echo -e "# Load pam_limits when using sudo -u command [auto-added by soem_master install.bash] \n$SUDO_PAM_LIMITS" | sudo tee --append $PAM_SUDO_FILE
+    echo -e "# Grant user amigo access to set realtime priorities [auto-added by soem_master install.bash] \n$REALTIME_LIMITS" | sudo tee --append $LIMITS_FILE
+fi
