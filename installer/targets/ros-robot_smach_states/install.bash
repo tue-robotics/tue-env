0cee51d48cf3fef3031599c2a33d98814ef5665e
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-06-03 10:24:04 +0200

    Removing pip check from robot_smach_states target
    Already in https://github.com/tue-robotics/tue-env/blob/master/installer/scripts/tue-install-impl#L661

--- installer/targets/ros-robot_smach_states/install.bash
+++ installer/targets/ros-robot_smach_states/install.bash
@@ -1,4 +1,1 @@
-# Make sure python-pip is installed
-dpkg -s python-pip &> /dev/null || sudo apt-get install python-pip
-
 dpkg -s ros-$TUE_ROS_DISTRO-orocos-kdl &> /dev/null && dpkg -s ros-$TUE_ROS_DISTRO-python-orocos-kdl &> /dev/null || sudo apt-get install -y ros-$TUE_ROS_DISTRO-orocos-kdl ros-$TUE_ROS_DISTRO-python-orocos-kdl
