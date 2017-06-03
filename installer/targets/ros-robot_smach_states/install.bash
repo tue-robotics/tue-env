c82c9ab0c54ca7fea71839f72c3ee41f31e9d587
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-06-03 10:39:33 +0200

    Dependencies transferred to package.xml

--- installer/targets/ros-robot_smach_states/install.bash
+++
@@ -1,1 +0,0 @@
-dpkg -s ros-$TUE_ROS_DISTRO-orocos-kdl &> /dev/null && dpkg -s ros-$TUE_ROS_DISTRO-python-orocos-kdl &> /dev/null || sudo apt-get install -y ros-$TUE_ROS_DISTRO-orocos-kdl ros-$TUE_ROS_DISTRO-python-orocos-kdl
