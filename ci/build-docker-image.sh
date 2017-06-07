84d2f67a15b2ed9e929f38356639e23922e2732c
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2017-06-07 14:25:28 +0200

    Also add better logging to docker build image script

--- ci/build-docker-image.sh
+++ ci/build-docker-image.sh
@@ -3,7 +3,7 @@
 # create tag based on branch name
 export IMAGE_NAME=tuerobotics/tue-env:`echo "$TRAVIS_BRANCH" | tr '[:upper:]' '[:lower:]' | sed -e 's:/:_:g'`
 
-echo "$IMAGE_NAME"
+echo -e "\e[35m\e[1m Creating docker $IMAGE_NAME \e[0m"
 
 # build the Docker image (this will use the Dockerfile in the root of the repo)
 docker build -t $IMAGE_NAME .
@@ -14,5 +14,6 @@ docker login -u="$DOCKER_HUB_USERNAME" -p="$DOCKER_HUB_PASSWORD"
 # push the new Docker image to the Docker registry only after acceptance of pull request
 if [[ $TRAVIS_PULL_REQUEST == “false” ]]; 
 then
+    echo -e "\e[35m\e[1m docker push $IMAGE_NAME \e[0m"
     docker push $IMAGE_NAME
 fi
