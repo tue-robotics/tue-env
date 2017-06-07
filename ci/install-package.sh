7a227c9103d708647598c00d73e6af35482973e0
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2017-06-07 07:23:57 +0200

    File name mixup

--- ci/install-package.sh
+++ ci/install-package.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-# This script can only be ran after the build-package.sh script
 
 for i in "$@"
 do
 case $i in
     -p=*|--package=*)
     PACKAGE="${i#*=}"
+    shift # past argument=value
+    ;;
+    -b=*|--branch=*)
+    BRANCH="${i#*=}"
+    shift # past argument=value
+    ;;
+    -c=*|--commit=*)
+    COMMIT="${i#*=}"
     shift # past argument=value
     ;;
     *)
@@ -15,6 +22,18 @@ esac
 done
 
 echo "PACKAGE     = ${PACKAGE}"
+echo "BRANCH      = ${BRANCH}"
+echo "COMMIT PATH = ${COMMIT}"
 
-# Compile the package
-docker exec tue-env bash -c "export CI='true'; source /home/amigo/.bashrc; cd ~/ros/kinetic/system/src/$REPO_NAME && catkin build --this --no-status"
+# Determine docker tag if the same branch exists there
+IMAGE_BRANCH_NAME=tuerobotics/tue-env:`echo "$BRANCH" | tr '[:upper:]' '[:lower:]' | sed -e 's:/:_:g'`
+
+# Set the default fallback branch to master
+IMAGE_MASTER_NAME=tuerobotics/tue-env:master
+
+# Run the docker image (user master as fallback)
+docker run --detach --interactive --name tue-env $IMAGE_BRANCH_NAME || docker run --detach --interactive --name tue-env $IMAGE_MASTER_NAME 
+
+# Install the pacakge
+  - docker exec tue-env bash -c "export CI='true'; source /home/amigo/.bashrc; tue-get install ros-$PACKAGE"
+  - docker exec tue-env bash -c "cd ~/ros/kinetic/system/src/$PACKAGE && git reset --hard $COMMIT"
