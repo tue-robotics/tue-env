360d5aa98bb7863ab16c438e99b1ace795db735c
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2017-06-09 11:13:58 +0200

    Include running tests on compiling

--- ci/build-package.sh
+++ ci/build-package.sh
@@ -20,3 +20,7 @@ echo -e "\e[35m\e[1m PACKAGE     = ${PACKAGE} \e[0m"
 # Compile the package
 echo -e "\e[35m\e[1m Compile the package \e[0m"
 docker exec tue-env bash -c "export CI='true'; source /home/amigo/.bashrc; cd ~/ros/kinetic/system/src/$PACKAGE && catkin build --this --no-status"
+
+# Run unit tests
+echo -e "\e[35m\e[1m Run unit test on this package (catkin run_test --this --no-deps \e[0m"
+docker exec tue-env bash -c "export CI='true'; source /home/amigo/.bashrc; cd ~/ros/kinetic/system/src/$PACKAGE && catkin run_tests --this --no-status --no-deps"
