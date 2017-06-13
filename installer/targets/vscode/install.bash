b8d7714c91aeff54616df3678e59803f9cb8c6af
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2017-06-13 21:50:42 +0200

    Forgot some assume yes files

--- installer/targets/vscode/install.bash
+++ installer/targets/vscode/install.bash
@@ -4,5 +4,5 @@ if [ ! -f /etc/apt/sources.list.d/vscode.list ]
 then
 	wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O /tmp/vscode.deb
 	sudo dpkg -i /tmp/vscode.deb
-	sudo apt-get install -f # Install dependencies
+	sudo apt-get install --assume-yes -f # Install dependencies
 fi
