73be6fa8f13be7c96cc9a3da06c2becefdb1dac1
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2017-08-03 14:43:35 +0200

    Enable passwordless sudo on robot and allow for serial devices (hokuyo etc)

--- installer/targets/robot/install.bash
+++ installer/targets/robot/install.bash
@@ -1,2 +1,16 @@
+#Do not require sudo
+if [ ! -f /etc/sudoers.d/sudo-no-password ]
+then
+    echo '%sudo ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/sudo-no-password && \
+    sudo chmod 440 /etc/sudoers.d/sudo-no-password
+fi
+
+### Make sure remote launching works ###
 tue-install-cp ros-bash-and-run.sh ~/.ros-bash-and-run.sh
 chmod +x ~/.ros-bash-and-run.sh
+
+### Allow user to access serial interfaces ###
+if ! groups ${USER} | grep -q dialout
+  then
+    sudo gpasswd --add ${USER} dialout
+fi
