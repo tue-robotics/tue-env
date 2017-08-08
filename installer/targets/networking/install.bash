64213a31fa71748a39afd30d3f679fdc7695670b
Tim Clephas <tim.clephas@tue.nl>
2017-08-08 14:57:55 +0200

    Prepare for using hostnames by adding networking target

---
+++ installer/targets/networking/install.bash
@@ -0,0 +1,11 @@
+## nsswitch
+set -e
+
+# Check config file
+if ! cmp /etc/nsswitch.conf ~/.tue/installer/targets/networking/nsswitch.conf --quiet
+then
+    echo "Chrony config is probably not correct, will copy"
+    
+    # Copy and backup old config
+    sudo install --backup=numbered --compare --verbose ~/.tue/installer/targets/networking/nsswitch.conf /etc/nsswitch.conf
+fi
