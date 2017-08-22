ce4f1a8e66d4a172968fa1be7c4e7a4671ded83a
Ramon Wijnands <rayman747@hotmail.com>
2017-08-22 20:33:31 +0200

    Cleanup the tensorflow installer

--- installer/targets/ros-tensorflow_ros/install.bash
+++ installer/targets/ros-tensorflow_ros/install.bash
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Make sure python-pip is installed
-dpkg -s python-pip &> /dev/null || sudo apt-get install python-pip -y
-
 if [ ! $(pip freeze | grep tensorflow) ]
 then
     export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0rc0-cp27-none-linux_x86_64.whl
@@ -10,7 +7,7 @@ then
     # ToDo: use pip target
     echo -e "\nInstalling tensorflow using pip.\n"
 
-    if [[ $(lsb_release -rs) > "16.03" ]] 
+    if [[ $(lsb_release -rs) > "16.03" ]]
     then
         pip install -U $TF_BINARY_URL --user
     else
