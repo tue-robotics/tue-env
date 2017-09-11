478764b7eb321c56eebb1be5a38711fb914a13d4
Tim Clephas <tim.clephas@tue.nl>
2017-09-11 09:19:32 +0200

    Remove passwordless sudo from robot target

--- installer/targets/robot/install.bash
+++ installer/targets/robot/install.bash
@@ -1,10 +1,3 @@
-#Do not require sudo
-if [ ! -f /etc/sudoers.d/sudo-no-password ]
-then
-    echo '%sudo ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/sudo-no-password && \
-    sudo chmod 440 /etc/sudoers.d/sudo-no-password
-fi
-
 ### Make sure remote launching works ###
 tue-install-cp ros-bash-and-run.sh ~/.ros-bash-and-run.sh
 chmod +x ~/.ros-bash-and-run.sh
