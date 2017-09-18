9ea4e6dcda84677e0b2b6c35843fca7aa49e1e33
Tim Clephas <tim.clephas@tue.nl>
2017-09-18 20:10:10 +0200

    Be consistent

--- installer/targets/networking/install.bash
+++ installer/targets/networking/install.bash
@@ -4,7 +4,7 @@ set -e
 # Check config file
 if ! cmp /etc/nsswitch.conf ~/.tue/installer/targets/networking/nsswitch.conf --quiet
 then
-    echo "Chrony config is probably not correct, will copy"
+    echo "nsswitch config is probably not correct, will copy"
     
     # Copy and backup old config
     sudo install --backup=numbered --compare --verbose ~/.tue/installer/targets/networking/nsswitch.conf /etc/nsswitch.conf
