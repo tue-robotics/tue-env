3638ffbec5e3a9306d0c8c639c42e0a0f5079f1c
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-09-27 11:32:08 +0200

    Update install.bash

--- installer/targets/matthijs/install.bash
+++ installer/targets/matthijs/install.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-diff=$(diff <(head -n 1 $TUE_DIR/installer/targets/matthijs/geany.conf) <(head -n 1 ~/.config/geany/geany.conf))
-if [ -n "$diff" ]
+file_diff=$(diff <(head -n 1 $TUE_DIR/installer/targets/matthijs/geany.conf) <(head -n 1 ~/.config/geany/geany.conf))
+if [ -n "$file_diff" ]
 then
     cp --verbose $TUE_DIR/installer/targets/matthijs/geany.conf  ~/.config/geany/geany.conf
 fi
