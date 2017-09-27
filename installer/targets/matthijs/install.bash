885c6e668435b7363159a9ace6bab0d3065d3792
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-09-27 14:37:15 +0200

    matthijs geany config, only when not there yet

--- installer/targets/matthijs/install.bash
+++ installer/targets/matthijs/install.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-file_diff=$(diff <(head -n 1 $TUE_DIR/installer/targets/matthijs/geany.conf) <(head -n 1 ~/.config/geany/geany.conf))
-if [ -n "$file_diff" ]
+if [ ! -f ~/.config/geany/geany.conf ]
 then
+    tue-install-debug "geany config not existing"
+    mkdir -p ~/.config/geany
     cp --verbose $TUE_DIR/installer/targets/matthijs/geany.conf  ~/.config/geany/geany.conf
+else
+    tue-install-debug "geany config does exists, so not copying matthhijs config"
 fi
