f679057a7a71b3033f1dc506c6a3e4dc00f7792f
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-09-27 10:24:42 +0200

    update install procedure terminator config

--- installer/targets/terminator/install.bash
+++ installer/targets/terminator/install.bash
@@ -1,10 +1,16 @@
-if [ -d ~/.config/terminator ]
+if [ ! -d ~/.config/terminator ]
 then
+    tue-install-debug "creating ~/.config/terminator, because not existing yet"
+    mkdir -p ~/.config/terminator
+else
+    tue-install-debug "~/.config/terminator already exists"
+fi
 
-    if [ -f ~/.config/terminator/config ]
-    then
-	    tue-install-cp config ~/.config/terminator/config2
-    else
-        tue-install-cp config ~/.config/terminator/config
-    fi
+if [ -f ~/.config/terminator/config ]
+then
+    tue-install-debug "tue-tue-install-cp config ~/.config/terminator/config2"
+    tue-install-cp config ~/.config/terminator/config2
+else
+    tue-install-debug "tue-tue-install-cp config ~/.config/terminator/config"
+    tue-install-cp config ~/.config/terminator/config
 fi
