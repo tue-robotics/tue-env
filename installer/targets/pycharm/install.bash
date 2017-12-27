89859c00a957729ca0961ff3572926af830185a8
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-12-27 16:18:24 +0100

    Warning instead of removing old pycharm directly

--- installer/targets/pycharm/install.bash
+++ installer/targets/pycharm/install.bash
@@ -4,7 +4,7 @@ pycharm="pycharm-community"
 if dpkg-query -W -f='${Status}' $pycharm 2>/dev/null | grep -q "ok installed"
 then
     tue-install-debug "Pycharm was installed  by apt, removing it now"
-    sudo apt-get remove $pycharm
+    tue-install-warning "Pycharm is now installed via SNAP. To remove the old apt version: sudo apt-get remove $pycharm"
 else
     tue-install-debug "Pycharm was not installed by apt"
 fi
