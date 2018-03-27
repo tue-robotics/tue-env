fa83b2d84f9a03457c96a2904083fb693cdf04fe
Tim Assman <t.m.assman@gmail.com>
2018-03-27 22:01:01 +0200

    rework from review during merge request

--- installer/targets/cmdgen/install.bash
+++ installer/targets/cmdgen/install.bash
@@ -1,37 +1,33 @@
 #!/bin/bash
 
-if [ ! -d ~/src/GPSRCmdGen ]
-then
-    tue-install-debug "GPSRCmdGen not yet installed"
-    echo "Installing GPSRCmdGen to ~/src/GPSRCmdGen"
+# by default, set the previous commit to -1, which will trigger a 'make'
+prev="-1"
 
-    echo "Installing dependency of GPSRCmdGen: mono-complete"
-    sudo apt-get install mono-complete
-
-    tue-install-git https://github.com/tue-robotics/GPSRCmdGen.git ~/src/GPSRCmdGen
-
-    echo "Making GPSRCmdGen"
+if [ -d ~/src/GPSRCmdGen ]; then # if the directory already exists
     cd ~/src/GPSRCmdGen
-    make
-
-else # GPSRCmdGen installed
-    cd ~/src/GPSRCmdGen
-    REMOTE=$(git config --get remote.origin.url)
+    REMOTE=$(git config --get remote.origin.url) # get the remote
+    
+    # if the GSPRCmdGen is pointing to the wrong Remote, correct it
     if [ "$REMOTE" != "https://github.com/tue-robotics/GPSRCmdGen.git" ]; then
-        echo "The GPSRCmdGen is still pointing to old remote, will be changed to tue-fork"
+        tue-install-debug "The GPSRCmdGen is still pointing to old remote, will be changed to tue-fork"
         git remote set-url origin https://github.com/tue-robotics/GPSRCmdGen.git
-    else
-      echo "The GPSRCmdGen is correctly pointing to tue-fork"
     fi
-    echo "Pulling GPSRCmdGen"
-    prev=$(git rev-list HEAD -n 1)
-    git pull origin master
 
-    # https://stackoverflow.com/questions/20995026/how-to-determine-whether-a-git-pull-did-something-from-a-shell-script
-    if [ "$prev" != "$(git rev-list HEAD -n 1)" ]; then
-      echo "Making GPSRCmdGen"
-      make
-    else
-      echo "Build already up-to-date"
-    fi
+    # Git is set-up correctly, so record the previous commit
+    prev=$(git rev-list HEAD -n 1)
+fi
+
+# tue-install-git will decide if clone or pull is needed
+tue-install-git https://github.com/tue-robotics/GPSRCmdGen.git ~/src/GPSRCmdGen
+
+# install mono if not yet installed
+hash mono 2> /dev/null || sudo apt-get install --assume-yes mono-complete
+
+# make if needed
+cd ~/src/GPSRCmdGen
+if [ "$prev" != "$(git rev-list HEAD -n 1)" ]; then
+    tue-install-debug "Making GPSRCmdGen"
+    make
+else
+    tue-install-debug "Build already up-to-date"
 fi
