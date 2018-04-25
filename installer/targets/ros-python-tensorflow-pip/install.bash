5d400733fd3421d5e486790bd1fb3a67d336ae43
Ramon Wijnands <rayman747@hotmail.com>
2018-04-25 14:06:17 +0200

    Revert tensorflow to 1.4.1
    1.7.* doesn't work. I know that this version does work.

--- installer/targets/ros-python-tensorflow-pip/install.bash
+++ installer/targets/ros-python-tensorflow-pip/install.bash
@@ -1,6 +1,6 @@
 if [ -n "$TUE_CUDA" ]
 then
-    tue-install-pip 'tensorflow-gpu>=1.4.1'
+    tue-install-pip 'tensorflow-gpu==1.4.1'
 else
-    tue-install-pip 'tensorflow>=1.4.1'
+    tue-install-pip 'tensorflow==1.4.1'
 fi
