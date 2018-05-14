1c9039b7983d38a3750d5a1be43da75059bf91cd
Tim Clephas <tim.clephas@nobleo.nl>
2018-05-14 15:55:51 +0200

    Convert install.bash lines to yaml

--- installer/targets/git/install.bash
+++ installer/targets/git/install.bash
@@ -8,8 +8,3 @@ if [ ! -f /usr/local/bin/git-extras ]; then
 	# enable color
 	git config --global color.ui true
 fi
-
-tue-install-ppa ppa:git-core/ppa #Git 2.9 required for git-hooks
-tue-install-system git
-tue-install-system gitk
-tue-install-system git-gui
