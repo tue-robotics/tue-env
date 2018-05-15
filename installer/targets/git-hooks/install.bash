bddc9edf9c0ab42eccb4126963cfd44383f30cfb
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2018-05-15 15:23:07 +0200

    typo and replace nobleo by tue

--- installer/targets/git-hooks/install.bash
+++ installer/targets/git-hooks/install.bash
@@ -1,2 +1,2 @@
 # install the global hooks
-git config --global core.hooksPath ~/.nobleo/installer/targets/git-hooks/git_hooks/
+git config --global core.hooksPath ~/.tue/installer/targets/git-hooks/git_hooks/
