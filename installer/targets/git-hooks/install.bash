1cd957ac7d815631b131f9bf08f36178edf6ee6a
Tim Clephas <tim.clephas@nobleo.nl>
2018-05-17 09:32:49 +0200

    Implement instructions for upgrading git

--- installer/targets/git-hooks/install.bash
+++ installer/targets/git-hooks/install.bash
@@ -1,2 +1,8 @@
 # install the global hooks
 git config --global core.hooksPath ~/.tue/installer/targets/git-hooks/git_hooks/
+
+if dpkg --compare-versions $(git --version | awk '{print $3}') lt 2.9; then
+    tue-install-warning "Please perform a
+    sudo apt install git
+afterwards to upgrade the git version to allow git-hooks"
+fi
