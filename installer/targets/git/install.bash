c74cbae06c6ebafa74be59f6fe02455f9c1a6950
Tim Clephas <tim.clephas@nobleo.nl>
2018-10-12 07:30:27 +0200

    Backport gitignore file if none exists

--- installer/targets/git/install.bash
+++ installer/targets/git/install.bash
@@ -2,9 +2,15 @@
 
 # check for git-extras
 if [ ! -f /usr/local/bin/git-extras ]; then
-	# https://github.com/tj/git-extras
-	cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && sudo make install
-
-	# enable color
-	git config --global color.ui true
+    # https://github.com/tj/git-extras
+    cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && sudo make install
 fi
+
+if [[ ! $(git config --global core.excludesfile) ]]; then
+    # enable global ignore file
+    git config --global core.excludesfile ~/.tue/installer/targets/git/gitignore_global
+fi
+
+### Some defaults:
+# enable color
+git config --global color.ui auto
