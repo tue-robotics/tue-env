31752521af73f32829d8fd72299c0899d6f4b50c
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2018-10-17 10:26:37 +0200

    Keep changes to minimum in git install.bash

--- installer/targets/git/install.bash
+++ installer/targets/git/install.bash
@@ -4,13 +4,12 @@
 if [ ! -f /usr/local/bin/git-extras ]; then
     # https://github.com/tj/git-extras
     cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && sudo make install
+    
+    # enable color
+    git config --global color.ui true
 fi
 
 if [[ ! $(git config --global core.excludesfile) ]]; then
     # enable global ignore file
-    git config --global core.excludesfile ~/.tue/installer/targets/git/gitignore_global
+    git config --global core.excludesfile $(dirname "${BASH_SOURCE[0]}")/gitignore_global
 fi
-
-### Some defaults:
-# enable color
-git config --global color.ui auto
