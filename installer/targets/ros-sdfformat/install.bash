246ebb7caba6d9c2dc160a24294eb1e2b50970ee
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2018-12-24 01:00:23 +0100

    Update install.bash

--- installer/targets/ros-sdfformat/install.bash
+++ installer/targets/ros-sdfformat/install.bash
@@ -1,14 +1,14 @@
 if [ ! -f /etc/apt/sources.list.d/gazebo-stable.list ]
 then
-    tue-install-debug "Adding Gazebo PPA"
+    tue-install-debug "Adding Gazebo source to apt-get"
     sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 
     wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 
     sudo apt-get update -qq
+    tue-install-debug "Succesfully added Gazebo source to apt-get"
 else
-    tue-install-debug "Gazebo PPA has already been added"
+    tue-install-debug "Gazebo source has already been added"
 fi
 
 tue-install-system-now libsdformat4-dev
-
