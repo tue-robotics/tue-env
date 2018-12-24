26f1f881ff09a9acccc200b2f62b3e2c9122b294
Arpit Aggarwal <ar13pit@gmail.com>
2018-12-24 18:16:27 +0100

    [TEMP] corrected git checkout dir

--- installer/scripts/bootstrap.bash
+++ installer/scripts/bootstrap.bash
@@ -33,7 +33,9 @@ if [[ -z "$CI" ]]
 then
     # Update installer
     git clone https://github.com/tue-robotics/tue-env.git ~/.tue
+    cd ~/.tue
     git checkout ar13pit-patch/fix-names-scripts-targets
+    cd
 fi
 
 # Source the installer commands
