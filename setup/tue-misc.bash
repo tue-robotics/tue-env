a9f7619dcdcd56611e42fcc04c7e4063dfc12816
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2018-12-28 12:05:05 +0100

    only complete pip, if installed

--- setup/tue-misc.bash
+++ setup/tue-misc.bash
@@ -4,11 +4,13 @@ export LC_ALL="C.UTF-8"
 
 # ------------------------------------------
 # pip bash completion
-
-_pip_completion()
-{
-    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
-                   COMP_CWORD=$COMP_CWORD \
-                   PIP_AUTO_COMPLETE=1 $1 ) )
-}
-complete -o default -F _pip_completion pip
+if hash pip 2> /dev/null
+then
+	_pip_completion()
+	{
+	    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
+	                   COMP_CWORD=$COMP_CWORD \
+	                   PIP_AUTO_COMPLETE=1 $1 ) )
+	}
+	complete -o default -F _pip_completion pip
+fi	
