488b133a97c818522724282c61ffdffbaeb6f9d1
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2018-12-28 11:58:54 +0100

    add pip completion to tue-misc

--- setup/tue-misc.bash
+++ setup/tue-misc.bash
@@ -1,3 +1,14 @@
 # -----------------------------------------
 # Fix annoying perl language warnings
 export LC_ALL="C.UTF-8"
+
+# ------------------------------------------
+# pip bash completion
+
+_pip_completion()
+{
+    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
+                   COMP_CWORD=$COMP_CWORD \
+                   PIP_AUTO_COMPLETE=1 $1 ) )
+}
+complete -o default -F _pip_completion pip
