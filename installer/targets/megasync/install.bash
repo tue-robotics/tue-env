f98bff4f50ab9d113b5989d95b0d626894ef1cb1
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-01-12 09:51:36 +0100

    apply tue-install-dpkg in megasync target

--- installer/targets/megasync/install.bash
+++ installer/targets/megasync/install.bash
@@ -2,20 +2,17 @@ DATA_DIR="$HOME/MEGA"
 if [ ! -d "$DATA_DIR" ]; then
     # Download mega sync
     UBUNTU_VERSION=`lsb_release -r | sed 's/Release:\s*\(.*\)/\1/'`
-    echo "Ubuntu version: $UBUNTU_VERSION"
+    tue-install-debug "Ubuntu version: $UBUNTU_VERSION"
 
     LINK="https://mega.nz/linux/MEGAsync/xUbuntu_""$UBUNTU_VERSION""/amd64/megasync-xUbuntu_""$UBUNTU_VERSION""_amd64.deb"
     DEST="/tmp/""$UBUNTU_VERSION""_MEGA_SYNC.deb"
 
-    echo "Downloading $LINK to $DEST"
+    tue-install-debug "Downloading $LINK to $DEST"
     wget $LINK -O $DEST
 
     # Install
-    echo "Installing debian package ..."
-    sudo dpkg -i $DEST
+    tue-install-debug "Installing debian package ..."
+    tue-install-dpkg $DEST
 
-    # Fix deps
-    sudo apt-get --assume-yes -f install
-
-    echo "NOW Please configure the mega client with the AMIGO credentials"
+    tue-install-info "NOW Please configure the mega client with the AMIGO credentials"
 fi
