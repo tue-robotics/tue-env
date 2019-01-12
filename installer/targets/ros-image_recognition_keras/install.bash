4a60950c8ad5058a63fc3505928da5f083ca4034
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-01-12 16:05:39 +0100

    move keras models to data folder

--- installer/targets/ros-image_recognition_keras/install.bash
+++ installer/targets/ros-image_recognition_keras/install.bash
@@ -1,6 +1,20 @@
 url="https://github.com/yu4u/age-gender-estimation/releases/download/v0.5/weights.28-3.73.hdf5"
-dest=~/src/keras_models/age_gender
+old_dest=~/src/keras_models/age_gender
+dest=~/data/keras_models/age_gender
+
+# move old dir to new destination
+if [ -d $old_dest ]
+then
+    mkdir -p $(dirname $dest)
+    mv $old_dest $dest
+    if [ -z "$(ls -A $(dirname $old_dest))" ]
+    then
+        rm -r $(dirname $old_dest)
+    fi
+fi
+
 if [ ! -d $dest ]
 then
+    tue-install-debug "wget $url -P $dest"
     wget $url -P $dest
 fi
