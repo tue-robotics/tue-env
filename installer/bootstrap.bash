862b168feaa6bb2f7af47ab823cfac2626a9018e
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-01-16 23:56:01 +0100

    add .git to url

--- installer/bootstrap.bash
+++ installer/bootstrap.bash
@@ -53,7 +53,7 @@ source ~/.tue/setup.bash
 mkdir -p ~/ros/$TUE_ROS_DISTRO
 
 # Initialize ros environment directory incl. targets
-tue-env init ros-$TUE_ROS_DISTRO ~/ros/$TUE_ROS_DISTRO https://github.com/tue-robotics/tue-env-targets
+tue-env init ros-$TUE_ROS_DISTRO ~/ros/$TUE_ROS_DISTRO https://github.com/tue-robotics/tue-env-targets.git
 
 # Set the correct ROS version for this environment
 mkdir -p ~/ros/$TUE_ROS_DISTRO/.env/setup
