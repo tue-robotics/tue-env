d8e9ae7f31caf2fa216b999d6ddb76f439a107e8
Arpit Aggarwal <ar13pit@gmail.com>
2019-02-13 14:19:03 +0100

    small cleanup

--- ci/build-docker-image.sh
+++ ci/build-docker-image.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-set -o errexit #Stop on errors
+#
+# Docker image builder
+# This script builds the docker image and pushes it to the Docker registry with
+# image tag as the branch name of the repository
+
+# Stop on errors
+set -o errexit
 
 # create tag based on branch name
 export IMAGE_NAME=tueroboticsamigo/tue-env:`echo "$TRAVIS_BRANCH" | tr '[:upper:]' '[:lower:]' | sed -e 's:/:_:g'`
@@ -13,11 +19,11 @@ docker build -t $IMAGE_NAME .
 docker login -u="$DOCKER_HUB_USERNAME" -p="$DOCKER_HUB_PASSWORD"
 
 # push the new Docker image to the Docker registry only after acceptance of pull request
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; 
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]
 then
     echo -e "\e[35m\e[1m docker push $IMAGE_NAME \e[0m"
     docker push $IMAGE_NAME
-    
+
     echo -e "\e[35m\e[1m Succeeded, see: \e[0m"
     docker images
 fi
