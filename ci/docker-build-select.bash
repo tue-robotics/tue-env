1b4ad3e9b6de47fcb8ff9535eb626c181618da75
Arpit Aggarwal <ar13pit@gmail.com>
2019-03-06 13:53:20 +0100

    added build select script with only Docker execution

---
+++ ci/docker-build-select.bash
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+#
+# Script to select the Docker build
+
+# Execute only inside the Docker
+if [ -z "$DOCKER" ]
+then
+    echo "Error! Script to be sourced inside a Docker image only"
+    return 1
+fi
+
+# If no PR is being tested, build the standard image
+if [ "$CI_PULL_REQUEST" == "false" ]
+then
+    source <(wget -O - https://raw.githubusercontent.com/tue-robotics/tue-env/"$CI_BRANCH"/installer/bootstrap.bash)
+else
+    [ -d ~/.tue ] && sudo chown -R "$USER":"$USER" ~/.tue && ~/.tue/installer/bootstrap.bash
+fi
+
