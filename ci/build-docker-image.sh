4711c001490f6a31c5fbf4d2af38d8588cd6a839
Arpit Aggarwal <ar13pit@gmail.com>
2019-03-11 21:03:01 +0100

    added commit as build arg

--- ci/build-docker-image.sh
+++ ci/build-docker-image.sh
@@ -19,24 +19,10 @@ IMAGE_NAME=tueroboticsamigo/tue-env:$(echo "$TRAVIS_BRANCH" | tr '[:upper:]' '[:
 
 echo -e "\e[35m\e[1m Creating docker $IMAGE_NAME \e[0m"
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-then
-    end_tag="##TUE_END"
-    begin_tag="##TUE_BEGIN"
-    target_file=Dockerfile
-    source_text="COPY / ./.tue/"
-
-    # Dynamically modify Dockerfile
-    sed -e "/^$end_tag/a $source_text" -e "/^$begin_tag/,/^$end_tag/d" $target_file | tee $target_file.tmp
-    mv $target_file.tmp $target_file
-
-    # Remove the dockerignore to ensure local repository copy
-    rm .dockerignore
-fi
-
 # build the Docker image (this will use the Dockerfile in the root of the repo)
 docker build --build-arg BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH} --build-arg \
-PULL_REQUEST="$TRAVIS_PULL_REQUEST" --build-arg CI="$CI" -t $IMAGE_NAME .
+PULL_REQUEST="$TRAVIS_PULL_REQUEST" --build-arg COMMIT="$TRAVIS_COMMIT" --build-arg \
+CI="$CI" -t $IMAGE_NAME .
 
 # push the new Docker image to the Docker registry only after acceptance of pull request
 if [ $TRAVIS_PULL_REQUEST == "false" ]
