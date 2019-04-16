c629d01e10557105b7338a33cbceec1d6451f123
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-16 09:26:14 +0200

    fix shellcheck for setup.bash

--- setup.bash
+++ setup.bash
@@ -1,8 +1,10 @@
 #! /usr/bin/env bash
-export TUE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TUE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export TUE_DIR
 
 # Load tue-env tool
-source $TUE_DIR/setup/tue-env.bash
+# shellcheck disable=SC1090
+source "$TUE_DIR"/setup/tue-env.bash
 
 # ------------------------------------------
 # Helper function for checking if all env vars are set
@@ -17,24 +19,26 @@ export -f _tue-check-env-vars
 
 if [ -z "$TUE_ENV" ]
 then
-    if [ ! -f $TUE_DIR/user/config/default_env ]
+    if [ ! -f "$TUE_DIR"/user/config/default_env ]
     then
         # No environment, so all environment specific setup below does not need to be sourced
-        return
+        return 0
     fi
 
-    export TUE_ENV=$(cat $TUE_DIR/user/config/default_env)
+    TUE_ENV=$(cat "$TUE_DIR"/user/config/default_env)
+    export TUE_ENV
 
-    if [ ! -f $TUE_DIR/user/envs/$TUE_ENV ]
+    if [ ! -f "$TUE_DIR"/user/envs/"$TUE_ENV" ]
     then
         echo "[tue] No such environment: '$TUE_ENV'"
-        return
+        return 1
     fi
 fi
 
-export TUE_ENV_DIR=$(cat $TUE_DIR/user/envs/$TUE_ENV)
+TUE_ENV_DIR=$(cat "$TUE_DIR"/user/envs/"$TUE_ENV")
+export TUE_ENV_DIR
 
-if [ ! -d $TUE_ENV_DIR ]
+if [ ! -d "$TUE_ENV_DIR" ]
 then
     echo "[tue] Environment directory '$TUE_ENV_DIR' (environment '$TUE_ENV') does not exist"
     return 1
@@ -42,29 +46,33 @@ fi
 
 export TUE_ENV_TARGETS_DIR=$TUE_ENV_DIR/.env/targets
 
-if [ ! -d $TUE_ENV_TARGETS_DIR ]
+if [ ! -d "$TUE_ENV_TARGETS_DIR" ]
 then
     echo "[tue] Targets directory '$TUE_ENV_TARGETS_DIR' (environment '$TUE_ENV') does not exist"
     return 1
 fi
 
-if [ -f $TUE_ENV_DIR/.env/setup/user_setup.bash ]
+if [ -f "$TUE_ENV_DIR"/.env/setup/user_setup.bash ]
 then
-    source $TUE_ENV_DIR/.env/setup/user_setup.bash
+    # shellcheck disable=SC1090
+    source "$TUE_ENV_DIR"/.env/setup/user_setup.bash
 fi
 
-if [ -f $TUE_ENV_DIR/.env/setup/target_setup.bash ]
+if [ -f "$TUE_ENV_DIR"/.env/setup/target_setup.bash ]
 then
-    source $TUE_ENV_DIR/.env/setup/target_setup.bash
+    # shellcheck disable=SC1090
+    source "$TUE_ENV_DIR"/.env/setup/target_setup.bash
 fi
 
 # -----------------------------------------
 # Load all the bash functions
-source $TUE_DIR/setup/tue-functions.bash
+# shellcheck disable=SC1090
+source "$TUE_DIR"/setup/tue-functions.bash
 
-if [ -f $TUE_DIR/setup/tue-misc.bash ]
+if [ -f "$TUE_DIR"/setup/tue-misc.bash ]
 then
-    source $TUE_DIR/setup/tue-misc.bash
+    # shellcheck disable=SC1090
+    source "$TUE_DIR"/setup/tue-misc.bash
 fi
 
 export TUE_BIN=$TUE_DIR/bin
