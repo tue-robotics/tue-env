c08c7d138ee0e22c98a461705835e67ad22aa84a
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-16 13:00:09 +0200

    fix shellcheck for ci/install-package.sh

--- ci/install-package.sh
+++ ci/install-package.sh
@@ -61,11 +61,12 @@ BRANCH_TAG=$(echo "$BRANCH" | tr '[:upper:]' '[:lower:]' | sed -e 's:/:_:g')
 MASTER_TAG=master
 
 # Remove any previously started containers if they exist (if not exist, still return true to let the script continue)
-docker stop tue-env  &> /dev/null || true && docker rm tue-env &> /dev/null || true
+docker stop tue-env  &> /dev/null || true
+docker rm tue-env &> /dev/null || true
 
 # Pull the identical branch name from dockerhub if exist, use master as fallback
 echo -e "\e[35m\e[1m Trying to fetch docker image: $IMAGE_NAME:$BRANCH_TAG \e[0m"
-if ! docker pull $IMAGE_NAME:$BRANCH_TAG
+if ! docker pull "$IMAGE_NAME:$BRANCH_TAG"
 then
     echo -e "\e[35m\e[1m No worries, we just test against the master branch: $IMAGE_NAME:$MASTER_TAG \e[0m"
     docker pull $IMAGE_NAME:$MASTER_TAG
@@ -73,7 +74,7 @@ then
 fi
 
 # Run the docker image along with setting new environment variables
-docker run --detach --interactive -e CI=true -e PACKAGE=$PACKAGE -e BRANCH=$BRANCH -e COMMIT=$COMMIT -e PULL_REQUEST=$PULL_REQUEST --name tue-env $IMAGE_NAME:$BRANCH_TAG
+docker run --detach --interactive -e CI=true -e PACKAGE="$PACKAGE" -e BRANCH="$BRANCH" -e COMMIT="$COMMIT" -e PULL_REQUEST="$PULL_REQUEST" --name tue-env "$IMAGE_NAME:$BRANCH_TAG"
 
 # Refresh the apt cache in the docker image
 docker exec tue-env bash -c "sudo apt-get update -qq"
