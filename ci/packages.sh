bbf0656884d0140004eb7d04d24f18d51176d9aa
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-04-16 13:24:03 +0200

    fix shellcheck for ci/packages.sh

--- ci/packages.sh
+++ ci/packages.sh
@@ -1,24 +1,25 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
-exclude_dirs=$(echo $@ | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}')
+exclude_dirs=$(echo "$@" | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}')
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]
 then
     diff_tag="HEAD^"
 else
-    diff_tag=$(git merge-base HEAD $TRAVIS_BRANCH)
+    diff_tag=$(git merge-base HEAD "$TRAVIS_BRANCH")
 fi
 
-dir_mod=$(git diff-tree --name-only HEAD $diff_tag | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}')
+dir_mod=$(git diff-tree --name-only HEAD "$diff_tag" | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}')
 if [ "$dir_mod" ]
 then
     PACKAGES=$dir_mod
 else
     # When no pkg is modified, build all pkgs (=all sub directories)
-    PACKAGES=$(ls -l | grep "^d" | awk '{print $NF}')
+    PACKAGES=$(printf "%s\n" ./*/ | cut -f2 -d '/')
 fi
 
-export PACKAGES=$(echo $PACKAGES | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}'| grep -v -w "$exclude_dirs")
+PACKAGES=$(echo "$PACKAGES" | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}'| grep -v -w "$exclude_dirs")
+export PACKAGES
 
 echo -e "\e[35m\e[1m PACKAGES: \e[0m"
 for PKG in $PACKAGES
