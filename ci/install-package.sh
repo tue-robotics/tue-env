38a38bf28d73b1a18517746228bdc0f29836abb2
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-08-11 09:13:35 +0200

    replace return by exit

--- ci/install-package.sh
+++ ci/install-package.sh
@@ -60,7 +60,7 @@ echo -e "\e[35m\e[1m PACKAGES     = ${PACKAGES} \e[0m"
 if [ -n "$PACKAGES" ] && ! echo "$PACKAGES" | grep -sqw "$PACKAGE"
 then
     echo -e "\e[35m\e[1m No changes in this package, so no need to run CI \e[0m"
-    return 0
+    exit 0
 fi
 
 # Name of the docker image
