1a4e2c15666b08128abdffe88a2ad4c40315186d
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-09-06 20:45:57 +0200

    create packages dict for azure

--- ci/packages.sh
+++ ci/packages.sh
@@ -21,6 +21,17 @@ fi
 PACKAGES=$(echo "$PACKAGES" | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}'| grep -v -w "$exclude_dirs")
 export PACKAGES
 
+PACKAGES_DICT="{"
+for PKG in $PACKAGES
+do
+	if [ $PACKAGES_DICT != "{" ]
+	then
+		PACKAGES_DICT+=", "
+	fi
+	PACKAGES_DICT+="{${PKG}: PACKAGE: ${PKG}}"
+done
+PACKAGES_DICT+="}"
+
 echo -e "\e[35m\e[1m PACKAGES: \e[0m"
 for PKG in $PACKAGES
 do
