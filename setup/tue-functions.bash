11494887ccd72019ca6ed6ff75e1e1ede7ced0c5
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-09-14 15:07:52 +0200

    [tue-apt-select-mirror] assume yes in apt install

--- setup/tue-functions.bash
+++ setup/tue-functions.bash
@@ -32,7 +32,7 @@ function tue-apt-select-mirror
     # It uses apt-select to generate a new sources.list, based on the current one.
     # All Arguments to this functions are passed on to apt-select, so check the
     # apt-select documentation for all options.
-	hash pip2 2> /dev/null|| sudo apt-get install python-pip
+	hash pip2 2> /dev/null|| sudo apt-get install --assume-yes python-pip
 	hash apt-select 2> /dev/null|| pip2 install --user apt-select
 
 	local mem_pwd=$PWD
