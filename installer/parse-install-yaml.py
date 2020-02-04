42808987b6fa9c9f156231be5411848467d25dc5
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-02-04 13:50:40 +0100

    (parse-install-yaml) cleanup

--- installer/parse-install-yaml.py
+++ installer/parse-install-yaml.py
@@ -1,31 +1,32 @@
-#!/usr/bin/python
+#! /usr/bin/env python
+
+from __future__ import print_function
 
 from os import environ
 import sys
 import yaml
 
+
 def show_error(error):
-    print "ERROR: {0}".format(error)
+    print("ERROR: {0}".format(error))
     return 1
+
 
 def main():
     if len(sys.argv) != 2:
-        print "Usage: parse-install-yaml install.yaml"
+        print("Usage: parse-install-yaml install.yaml")
         return 1
 
-    f = open(sys.argv[1])
-
-    try:
-        install_items = yaml.safe_load(f)
-    except yaml.parser.ParserError:
-        return show_error("Invalid yaml file.")
-
-    f.close()
+    with open(sys.argv[1]) as f:
+        try:
+            install_items = yaml.safe_load(f)
+        except (yaml.parser.ParserError, yaml.scanner.ScannerError) as e:
+            return show_error("Invalid yaml syntax: {0}".format(e))
 
     if not isinstance(install_items, list):
-        return show_error("install file should be a YAML sequence")
+        return show_error("Root of install.yaml file should be a YAML sequence")
 
-    commands=""
+    commands = []
 
     for install_item in install_items:
         command = None
@@ -44,7 +45,7 @@ def main():
                     elif "default" in install_item:
                         source = install_item["default"]["source"]
                     else:
-                        return show_error("ROS distro "+release+" or 'default' not specified in install.yaml")
+                        return show_error("ROS distro {} or 'default' not specified in install.yaml".format(release))
 
                 source_type = source["type"]
                 if source_type == "svn" or source_type == "git":
@@ -55,9 +56,11 @@ def main():
 
                     command = "tue-install-{0} {1} {2} {3}".format(install_type, source_type, source["url"], sub_dir)
                     if "version" in source:
-                       command += " {0}".format(source["version"])
+                        command += " {0}".format(source["version"])
                 elif source_type == "system":
                     command = "tue-install-ros system {0}".format(source["name"])
+                else:
+                    return show_error("Unknown ROS install type: '{0}'".format(source_type))
             elif install_type == "target":
                 command = "tue-install-target {0}".format(install_item["name"])
             elif install_type == "system":
@@ -73,19 +76,21 @@ def main():
             elif install_type == "dpkg":
                 command = "tue-install-dpkg {0}".format(install_item["name"])
             else:
-               return show_error("Unknown install type: '{0}'".format(install_type))
+                return show_error("Unknown install type: '{0}'".format(install_type))
 
         except KeyError as e:
-            return show_error("invalid install file: Key {0} could not be found.".format(e))
+            return show_error("Invalid install.yaml file: Key {0} could not be found.".format(e))
 
         if not command:
-            return show_error("invalid install file")
+            return show_error("Invalid install.yaml file")
 
         command = command.replace(" ", "^")
+        commands.append(command)
 
-        commands += command + " "
+    commands = " ".join(commands)
 
-    print commands
+    print(commands)
+
 
 if __name__ == "__main__":
     sys.exit(main())
