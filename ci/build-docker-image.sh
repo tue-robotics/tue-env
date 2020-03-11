d19ff582ce5432bcd7e74c28b0db864cb9a9ba3b
Arpit Aggarwal <ar13pit@gmail.com>
2020-03-11 14:14:12 +0100

    feat(ci): Parametrize build docker image and add support for ssh

--- ci/build-docker-image.sh
+++ ci/build-docker-image.sh
@@ -14,14 +14,44 @@ then
     exit 1
 fi
 
-if [ -z "$1" ]
-then
-    echo -e "Error! Need image tag subname as input."
-    exit 1
-fi
+for i in "$@"
+do
+    case $i in
+        -i=* | --image=* )
+            CI_DOCKER_IMAGE_NAME="${i#*=}" ;;
 
-IMAGE_NAME_SUBSTRING="$1"
-case $IMAGE_NAME_SUBSTRING in
+        -b=* | --branch=* )
+            CI_BRANCH="${i#*=}" ;;
+
+        -pr=* | --pull_request=* )
+            CI_PULL_REQUEST="${i#*=}" ;;
+
+        -prb=* | --pull_request_branch=* )
+            CI_PULL_REQUEST_BRANCH="${i#*=}" ;;
+
+        -c=* | --commit=* )
+            CI_COMMIT="${i#*=}" ;;
+
+        -u=* | --user=* )
+            CI_DOCKER_USER="${i#*=}" ;;
+
+        -p=* | --password=* )
+            CI_DOCKER_PASSWORD="${i#*=}" ;;
+
+        -r=* | --registry=* )
+            CI_DOCKER_REGISTRY="${i#*=}" ;;
+
+        -s | --ssh )
+            CI_DOCKER_SSH=true ;;
+
+        * )
+            echo -e "Error! Unknown input variable"
+            exit 1 ;;
+    esac
+done
+
+image_substring="$(basename $CI_DOCKER_IMAGE_NAME)"
+case $image_substring in
     tue-env )
         BASE_IMAGE="ubuntu:16.04" ;;
     tue-env-cuda )
@@ -33,23 +63,30 @@ case $IMAGE_NAME_SUBSTRING in
 esac
 
 # Create tag based on branch name
-IMAGE_NAME=tuerobotics/"$IMAGE_NAME_SUBSTRING":$(echo "$TRAVIS_BRANCH" | tr '[:upper:]' '[:lower:]' | sed -e 's:/:_:g')
-
-echo -e "\e[35m\e[1m Creating docker $IMAGE_NAME \e[0m"
+CI_DOCKER_IMAGE_NAME="$CI_DOCKER_IMAGE_NAME":$(echo "$CI_BRANCH" | tr '[:upper:]' '[:lower:]' | sed -e 's:/:_:g')
+echo -e "\e[35m\e[1m Creating docker $CI_DOCKER_IMAGE_NAME \e[0m"
 
 # build the Docker image (this will use the Dockerfile in the root of the repo)
-docker build --build-arg BRANCH="${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" --build-arg \
-PULL_REQUEST="$TRAVIS_PULL_REQUEST" --build-arg COMMIT="$TRAVIS_COMMIT" --build-arg \
-CI="$CI" --build-arg BASE_IMAGE="$BASE_IMAGE" -t "$IMAGE_NAME" .
+if [ "$CI_DOCKER_SSH" == "true" ]
+then
+    DOCKER_BUILDKIT=1 docker build --ssh=default --build-arg BRANCH="${CI_PULL_REQUEST_BRANCH:-$CI_BRANCH}" --build-arg \
+        PULL_REQUEST="$CI_PULL_REQUEST" --build-arg COMMIT="$CI_COMMIT" --build-arg \
+        CI="$CI" --build-arg BASE_IMAGE="$BASE_IMAGE" -t "$CI_DOCKER_IMAGE_NAME" .
+else
+    docker build --build-arg BRANCH="${CI_PULL_REQUEST_BRANCH:-$CI_BRANCH}" --build-arg \
+        PULL_REQUEST="$CI_PULL_REQUEST" --build-arg COMMIT="$CI_COMMIT" --build-arg \
+        CI="$CI" --build-arg BASE_IMAGE="$BASE_IMAGE" -t "$CI_DOCKER_IMAGE_NAME" .
+fi
 
 # push the new Docker image to the Docker registry only after acceptance of pull request
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+if [ "$CI_PULL_REQUEST" == "false" ]
 then
-    # authenticate with the Docker Hub registry
-    echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
+    # Authenticate to the Docker registry
+    echo -e "\e[35m\e[1m Authenticating docker registry $CI_DOCKER_REGISTRY \e[0m"
+    echo "$CI_DOCKER_PASSWORD" | docker login "$CI_DOCKER_REGISTRY" -u "$CI_DOCKER_USER" --password-stdin
 
-    echo -e "\e[35m\e[1m docker push $IMAGE_NAME \e[0m"
-    docker push "$IMAGE_NAME"
+    echo -e "\e[35m\e[1m docker push $CI_DOCKER_IMAGE_NAME \e[0m"
+    docker push "$CI_DOCKER_IMAGE_NAME"
 
     echo -e "\e[35m\e[1m Succeeded, see: \e[0m"
     docker images
