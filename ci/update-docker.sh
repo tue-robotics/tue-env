1b57db3577594bd46aa6ca06369d1674e3b09e37
Arpit Aggarwal <ar13pit@gmail.com>
2020-03-13 00:23:39 +0100

    feat: Keep apt quiet

--- ci/update-docker.sh
+++ ci/update-docker.sh
@@ -11,10 +11,10 @@ then
 fi
 
 # Download latest stable docker version
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - > /dev/null
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
+sudo apt-get update -qq
+sudo apt-get -y -qq -o Dpkg::Options::="--force-confnew" install docker-ce > /dev/null
 
 # This is need on travis because of https://github.com/moby/moby/issues/39120 and https://github.com/travis-ci/travis-ci/issues/6418
 sudo rm /etc/docker/daemon.json
