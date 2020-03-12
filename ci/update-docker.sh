eea396cd0242e4292a4787b34d44759c975f3505
Arpit Aggarwal <ar13pit@gmail.com>
2020-03-13 00:22:34 +0100

    feat: Enforce run only in CI

--- ci/update-docker.sh
+++ ci/update-docker.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/env sh
 
+# Stop on errors
+set -o errexit
+
+# Execute script only in a CI environment
+if [ "$CI" != "true" ]
+then
+    echo -e "\e[35m\e[1m Error!\e[0m Trying to execute a CI script in a non-CI environment. Exiting script."
+    exit 1
+fi
+
 # Download latest stable docker version
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
