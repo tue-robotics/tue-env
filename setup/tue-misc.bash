b3918022fab49adba743d197849334f4b36d3aa5
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-03-13 13:39:18 +0100

    Update pip completion

--- setup/tue-misc.bash
+++ setup/tue-misc.bash
@@ -6,14 +6,18 @@ export LC_ALL="C.UTF-8"
 
 # ------------------------------------------
 # pip bash completion
-if hash pip 2> /dev/null
+if hash pip 2> /dev/null || hash pip2 2> /dev/null || hash pip3 2> /dev/null
 then
     _pip_completion()
     {
         # shellcheck disable=SC2207
         COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
                        COMP_CWORD=$COMP_CWORD \
-                       PIP_AUTO_COMPLETE=1 $1 ) )
+                       PIP_AUTO_COMPLETE=1 $1 2>/dev/null ) )
     }
     complete -o default -F _pip_completion pip
 fi
+
+hash pip 2> /dev/null && complete -o default -F _pip_completion pip
+hash pip2 2> /dev/null && complete -o default -F _pip_completion pip2
+hash pip3 2> /dev/null && complete -o default -F _pip_completion pip3
