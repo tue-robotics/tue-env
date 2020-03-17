538060e3acf675445f7b3e46274e8131b81dbd41
Arpit Aggarwal <ar13pit@gmail.com>
2020-03-17 20:04:44 +0100

    feat: Keep previous use-ssh functions for setting default behavior

--- setup/tue-env-config.bash
+++ setup/tue-env-config.bash
@@ -18,6 +18,24 @@ function _set_export_option
         -e '$a'"export ${key}"'='"${value}" "$3"
 }
 
+function tue-env-use-ssh
+{
+    local option="TUE_USE_SSH"
+    local value="true"
+    _set_export_option "$option" "$value" "$tue_env_dir"/.env/setup/user_setup.bash
+
+    echo -e "[tue-env](config) Environment '$env' set to use SSH"
+}
+
+function tue-env-use-https
+{
+    local option="TUE_USE_SSH"
+    local value="false"
+    _set_export_option "$option" "$value" "$tue_env_dir"/.env/setup/user_setup.bash
+
+    echo -e "[tue-env](config) Environment '$env' set to use HTTPS"
+}
+
 function tue-env-github-use-ssh
 {
     local option="TUE_GITHUB_USE_SSH"
