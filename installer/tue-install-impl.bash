7323f8be74f52aec2737b02fc15c1f2154982645
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-04-11 01:22:20 +0200

    rename parse-ros-package-deps.py -> parse-package-xml.py

--- installer/tue-install-impl.bash
+++ installer/tue-install-impl.bash
@@ -1139,7 +1139,7 @@ function tue-install-ros
                 # Catkin
                 tue-install-debug "Parsing $pkg_xml"
                 local deps
-                deps=$("$TUE_INSTALL_SCRIPTS_DIR"/parse-ros-package-deps.py "$pkg_xml")
+                deps=$("$TUE_INSTALL_SCRIPTS_DIR"/parse-package-xml.py "$pkg_xml")
                 tue-install-debug "Parsed package.xml\n$deps"
 
                 for dep in $deps
