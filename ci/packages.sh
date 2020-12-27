c7bf4bb88ccdbdba651124c92204c2ea0be5c35f
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-12-27 10:36:29 +0100

    Use brackets around variables in ci/packages.sh

--- ci/packages.sh
+++ ci/packages.sh
@@ -16,7 +16,7 @@ do
         -a=* | --all=* )
             ALL="${i#*=}"
         ;;
-        * ) exclude_dirs="$exclude_dirs $1"
+        * ) exclude_dirs="${exclude_dirs} $1"
         ;;
     esac
     shift
@@ -28,7 +28,7 @@ echo -e "\e[35m\e[1m BRANCH       = ${BRANCH} \e[0m"
 echo -e "\e[35m\e[1m ALL          = ${ALL} \e[0m"
 echo -e "\e[35m\e[1m exclude_dirs = ${exclude_dirs} \e[0m"
 
-exclude_dirs=$(echo "$exclude_dirs" | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}')
+exclude_dirs=$(echo "${exclude_dirs}" | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}')
 
 if [ -n "$COMMIT_RANGE" ]
 then
@@ -37,40 +37,40 @@ then
     echo -e "\e[35m\e[1m Oldest commit: ${oldest_commit}\e[0m"
     echo -e "\e[35m\e[1m Newest commit: ${newest_commit}\e[0m"
 
-    mod_files=$(git diff-tree --name-only "$oldest_commit" "$newest_commit")
+    mod_files=$(git diff-tree --name-only "${oldest_commit}" "${newest_commit}")
 else
     # For compatibility
     if [[ $PULL_REQUEST == "false" ]]
     then
         diff_tag="HEAD^"
     else
-        diff_tag=$(git merge-base HEAD remotes/origin/"$BRANCH")
+        diff_tag=$(git merge-base HEAD remotes/origin/"${BRANCH}")
     fi
 
-    mod_files=$(git diff-tree --name-only HEAD "$diff_tag")
+    mod_files=$(git diff-tree --name-only HEAD "${diff_tag}")
 fi
 
-dir_mod=$(echo "$mod_files" | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}')
+dir_mod=$(echo "${mod_files}" | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}')
 
-if [[ $mod_files == *".travis.yml"* ]] || [[ $mod_files == *"azure-pipelines.yml"* ]] || [[ $mod_files == *".github"* ]] || [ "$ALL" == "true" ]
+if [[ ${mod_files} == *".travis.yml"* ]] || [[ ${mod_files} == *"azure-pipelines.yml"* ]] || [[ ${mod_files} == *".github"* ]] || [ "$ALL" == "true" ]
 then
     # If CI config is modified, build all pkgs (=all sub directories)
     PACKAGES=$(printf "%s\n" ./*/ | cut -f2 -d '/')
-elif [ "$dir_mod" ]
+elif [ "${dir_mod}" ]
 then
     # Else only the modified pkgs
-    PACKAGES=$dir_mod
+    PACKAGES=${dir_mod}
 else
     PACKAGES=""
 fi
 
-PACKAGES=$(echo "$PACKAGES" | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}'| grep -v -w "$exclude_dirs")
+PACKAGES=$(echo "${PACKAGES}" | xargs ls -dl 2>/dev/null |  grep "^d" | grep -v "\." | awk '{print $NF}'| grep -v -w "${exclude_dirs}")
 export PACKAGES
 
 PACKAGES_DICT="{"
 for PKG in $PACKAGES
 do
-    if [[ "$PACKAGES_DICT" != "{" ]]
+    if [[ "${PACKAGES_DICT}" != "{" ]]
     then
         PACKAGES_DICT+=", "
     fi
@@ -82,7 +82,7 @@ export PACKAGES_DICT
 PACKAGES_LIST="["
 for PKG in $PACKAGES
 do
-    if [[ "$PACKAGES_LIST" != "[" ]]
+    if [[ "${PACKAGES_LIST}" != "[" ]]
     then
         PACKAGES_LIST+=", "
     fi
@@ -94,5 +94,5 @@ export PACKAGES_LIST
 echo -e "\e[35m\e[1m PACKAGES: \e[0m"
 for PKG in $PACKAGES
 do
-    echo -e "\e[35m\e[1m $PKG \e[0m"
+    echo -e "\e[35m\e[1m ${PKG} \e[0m"
 done
