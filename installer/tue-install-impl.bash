4bfa39a1c1cd5c140c2ea66ad55d80feaff23286
Arpit Aggarwal <ar13pit@gmail.com>
2021-01-27 01:32:08 +0100

    refactor: Remove support for hg

--- installer/tue-install-impl.bash
+++ installer/tue-install-impl.bash
@@ -27,6 +27,16 @@ TUE_REPOS_DIR=$TUE_ENV_DIR/repos
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+function _skip_in_ci
+{
+    # use as followed:
+    # _skip_in_ci && return 0
+    [[ "$CI" == "true" ]] && return 0
+    return 1
+}
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 function date_stamp
 {
     date +%Y_%m_%d_%H_%M_%S
@@ -376,117 +386,6 @@ function tue-install-git
 
     _show_update_message "$TUE_INSTALL_CURRENT_TARGET" "$res"
 }
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-function tue-install-hg
-{
-    tue-install-debug "tue-install-hg $*"
-
-    local repo=$1
-    local targetdir=$2
-    local version=$3
-
-    # Mercurial config extension to write configs from cli
-    local hgcfg_folder="$HOME"/src/hgcfg
-    local hgcfg_pulled=/tmp/tue_get_hgcfg_pulled
-    if [ ! -f "$hgcfg_pulled" ]
-    then
-        parent_target=$TUE_INSTALL_CURRENT_TARGET
-        TUE_INSTALL_CURRENT_TARGET="hgcfg"
-        tue-install-git "https://github.com/tue-robotics/hgconfig.git" "$hgcfg_folder"
-        TUE_INSTALL_CURRENT_TARGET=$parent_target
-        if [ -z "$(hg config extensions.hgcfg)" ]
-        then
-            echo -e "\n[extensions]" >> ~/.hgrc
-            echo -e "hgcfg = $hgcfg_folder/hgext/hgcfg.py" >> ~/.hgrc
-            hg cfg --user config.delete_on_replace True
-        fi
-        touch $hgcfg_pulled
-    fi
-
-    if [ ! -d "$targetdir" ]
-    then
-        tue-install-debug "hg clone $repo $targetdir"
-        res=$(hg clone "$repo" "$targetdir" 2>&1)
-        TUE_INSTALL_HG_PULL_Q+=:$targetdir:
-    else
-        # Check if we have already pulled the repo
-        if [[ $TUE_INSTALL_HG_PULL_Q == *:$targetdir:* ]]
-        then
-            tue-install-debug "Repo previously pulled, skipping"
-            # We have already pulled this repo, skip it
-            res=
-        else
-            # Switch url of origin to use https/ssh if different
-            # Get current remote url
-            local current_url
-            current_url=$(hg -R "$targetdir" cfg paths.default | awk '{print $2}')
-
-            # If different, switch url
-            if [ "$current_url" != "$repo" ]
-            then
-                tue-install-debug "hg -R $targetdir config paths.default $repo"
-                hg -R "$targetdir" config paths.default "$repo"
-                tue-install-info "URL has switched to $repo"
-            fi
-
-            tue-install-debug "hg -R $targetdir pull -u"
-
-            local res
-            res=$(hg -R "$targetdir" pull -u 2>&1)
-
-            tue-install-debug "$res"
-
-            TUE_INSTALL_HG_PULL_Q+=:$targetdir:
-
-            if [[ $res == *"no changes found" ]]
-            then
-                res=
-            fi
-        fi
-    fi
-
-    tue-install-debug "Desired version: $version"
-    local _try_branch_res # Will be used in _try_branch_hg
-    if [ -n "$version" ]
-    then
-        _try_branch_res=""
-        _try_branch_hg "$targetdir" "$version"
-        [ -n "$_try_branch_res" ] && res="${res:+${res} }$_try_branch_res"
-    fi
-
-    tue-install-debug "Desired branch: $BRANCH"
-    if [ -n "$BRANCH" ] # Cannot be combined with version-if because this one might not exist
-    then
-        _try_branch_res=""
-        _try_branch_hg "$targetdir" "$BRANCH"
-        [ -n "$_try_branch_res" ] && res="${res:+${res} }$_try_branch_res"
-    fi
-
-    _show_update_message "$TUE_INSTALL_CURRENT_TARGET" "$res"
-}
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-function _try_branch_hg
-{
-    tue-install-debug "_try_branch_hg $*"
-
-    if [ -z "$2" ]
-    then
-        tue-install-error "Invalid _try_branch_hg: needs two arguments (repo and branch)."
-    fi
-
-    tue-install-debug "hg -R $1 checkout $2"
-    _try_branch_res=$(hg -R "$1" checkout "$2" 2>&1) # This is a "global" variable from tue-install-hg
-    tue-install-debug "_try_branch_res: $_try_branch_res"
-    if [[ $_try_branch_res == "1 files updated, 0 files merged, 1 files removed, 0 files unresolved" || $_try_branch_res == "abort: unknown revision"* ]]
-    then
-        _try_branch_res=
-    fi
-}
-
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -1328,7 +1227,7 @@ TUE_INSTALL_INFOS=
 
 tue-install-system-now git python3-pip python-is-python3
 
-tue-install-pip3-now catkin-pkg PyYAML "mercurial>=5.3"
+tue-install-pip3-now catkin-pkg PyYAML
 
 
 # Handling of targets
