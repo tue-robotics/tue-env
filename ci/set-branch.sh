2509bfb23cf3a5ff79e8b911cc7546f9a53f9dda
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-08-04 14:46:32 +0200

    (CI) reduce whitespace in logging

--- ci/set-branch.sh
+++ ci/set-branch.sh
@@ -14,29 +14,29 @@ do
         # unknown option
         if [[ -n "$i" ]]
         then
-            echo -e "\e[35m\e[1m Unknown input argument '$i'. Check CI .yml file\e[0m"
+            echo -e "\e[35m\e[1mUnknown input argument '$i'. Check CI .yml file\e[0m"
             exit 1
         fi ;;
     esac
     shift
 done
 
-echo -e "\e[35m\e[1m REPO   = ${REPO}\e[0m"
-echo -e "\e[35m\e[1m BRANCH = ${BRANCH}\e[0m"
+echo -e "\e[35m\e[1mREPO   = ${REPO}\e[0m"
+echo -e "\e[35m\e[1mBRANCH = ${BRANCH}\e[0m"
 
-echo -e "\e[35m\e[1m Getting default branch by GitHub API of: ${REPO}\e[0m"
+echo -e "\e[35m\e[1mGetting default branch by GitHub API of: ${REPO}\e[0m"
 
 DEFAULT_BRANCH=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"${REPO}" 2>/dev/null | jq ".default_branch" -r)
 
-echo -e "\e[35m\e[1m BRANCH         = ${BRANCH}\e[0m"
-echo -e "\e[35m\e[1m DEFAULT_BRANCH = ${DEFAULT_BRANCH}\e[0m"
+echo -e "\e[35m\e[1mBRANCH         = ${BRANCH}\e[0m"
+echo -e "\e[35m\e[1mDEFAULT_BRANCH = ${DEFAULT_BRANCH}\e[0m"
 
 if [[ "$BRANCH" == "$DEFAULT_BRANCH" ]]
 then
-    echo -e "\e[35m\e[1m On the default branch, clearing BRANCH variable\e[0m"
+    echo -e "\e[35m\e[1mOn the default branch, clearing BRANCH variable\e[0m"
     BRANCH=""
 else
-    echo -e "\e[35m\e[1m On a feature branch, keeping the BRANCH variable: ${BRANCH}\e[0m"
+    echo -e "\e[35m\e[1mOn a feature branch, keeping the BRANCH variable: ${BRANCH}\e[0m"
 fi
 
 export BRANCh
--- ci/test-package.sh
+++ ci/test-package.sh
@@ -10,7 +10,7 @@ set -o errexit
 # Execute script only in a CI environment
 if [ "$CI" != "true" ]
 then
-    echo -e "\e[35m\e[1m Error!\e[0m Trying to execute a CI script in a non-CI environment. Exiting script."
+    echo -e "\e[35m\e[1mError!\e[0mTrying to execute a CI script in a non-CI environment. Exiting script."
     exit 1
 fi
 
@@ -23,27 +23,27 @@ do
 
         * )
             # unknown option
-            echo -e "\e[35m\e[1m Unknown input argument '$i'. Check CI .yml file \e[0m"
+            echo -e "\e[35m\e[1mUnknown input argument '$i'. Check CI .yml file\e[0m"
             exit 1 ;;
     esac
     shift
 done
 
-echo -e "\e[35m\e[1m PACKAGE     = ${PACKAGE} \e[0m"
+echo -e "\e[35m\e[1mPACKAGE     = ${PACKAGE}\e[0m"
 
 # If packages is non-zero, this is a multi-package repo. In multi-package repo, check if this package needs CI.
 # If a single-package repo, CI is always needed.
 # shellcheck disable=SC2153
 if [ -n "$PACKAGES" ] && ! echo "$PACKAGES" | grep -sqw "$PACKAGE"
 then
-    echo -e "\e[35m\e[1m No changes in this package, so no need to run CI \e[0m"
+    echo -e "\e[35m\e[1mNo changes in this package, so no need to run CI\e[0m"
     exit 0
 fi
 
 # Run unit tests
-echo -e "\e[35m\e[1m Run tests on this package (catkin run_tests --this --no-deps -DCATKIN_ENABLE_TESTING=ON) \e[0m"
+echo -e "\e[35m\e[1mRun tests on this package (catkin run_tests --this --no-deps -DCATKIN_ENABLE_TESTING=ON)\e[0m"
 docker exec -t tue-env bash -c 'source ~/.bashrc; cd "$TUE_SYSTEM_DIR"/src/"$PACKAGE" && catkin run_tests --this --no-status --no-deps -DCATKIN_ENABLE_TESTING=ON'
 
 # Check results of unit tests
-echo -e "\e[35m\e[1m Check results of the tests on this package (catkin_test_results build/$PACKAGE) \e[0m"
+echo -e "\e[35m\e[1mCheck results of the tests on this package (catkin_test_results build/$PACKAGE)\e[0m"
 docker exec -t tue-env bash -c 'source ~/.bashrc; cd "$TUE_SYSTEM_DIR" && [ ! -d build/"$PACKAGE" ] || catkin_test_results build/"$PACKAGE"'
