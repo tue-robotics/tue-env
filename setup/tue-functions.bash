8c0eac77ea0df061a5a315912d6beabd24bdbc93
Arpit Aggarwal <ar13pit@gmail.com>
2022-05-20 13:30:22 +0200

    SPANAR-65 feat: Pass all args to colcon build

--- setup/tue-functions.bash
+++ setup/tue-functions.bash
@@ -409,7 +409,7 @@ function tue-make
     elif [ "${ros_version}" == "2" ]
     then
         mkdir -p "$TUE_SYSTEM_DIR"/src
-        colcon --log-base "$TUE_SYSTEM_DIR"/log build --merge-install --symlink-install --base-paths "$TUE_SYSTEM_DIR"/src --build-base "$TUE_SYSTEM_DIR"/build --install-base "$TUE_SYSTEM_DIR"/install
+        colcon --log-base "$TUE_SYSTEM_DIR"/log build --merge-install --symlink-install --base-paths "$TUE_SYSTEM_DIR"/src --build-base "$TUE_SYSTEM_DIR"/build --install-base "$TUE_SYSTEM_DIR"/install "$@"
         return $?
     else
         echo -e "\033[38;1mError! ROS_VERSION '${ros_version}' is not supported by tue-env.\033[0m"
