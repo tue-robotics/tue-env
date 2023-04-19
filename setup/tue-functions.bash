4b9acc48961e29b777af61f3d6f6e91aa9ccca44
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-04-19 09:32:12 +0200

    Set RoboCup timezone to Lisbon

--- setup/tue-functions.bash
+++ setup/tue-functions.bash
@@ -1664,7 +1664,7 @@ function tue-robocup-set-roboticssrv
 
 function tue-robocup-set-timezone-robocup
 {
-    sudo timedatectl set-timezone Asia/Bangkok
+    sudo timedatectl set-timezone Europe/Lisbon
 }
 
 function tue-robocup-set-timezone-home
