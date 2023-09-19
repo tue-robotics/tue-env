9c77b4707c0e7ca07e610f8739b340bf2c2273c3
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2023-09-19 14:58:39 +0200

    (CI)(organization-packages) working version

--- ci/organization-packages.sh
+++ ci/organization-packages.sh
@@ -9,6 +9,9 @@ do
         -t=* | --type=* )
             PACKAGE_TYPE="${i#*=}"
             ;;
+        --token=* )
+            TOKEN="${i#*=}"
+            ;;
         * )
             # unknown option
             if [[ -n "${i}" ]]  # Ignore empty arguments
@@ -23,27 +26,20 @@ done
 
 echo -e "\e[35m\e[1mORGANIZATION = ${ORGANIZATION}\e[0m"
 echo -e "\e[35m\e[1mPACKAGE_TYPE = ${PACKAGE_TYPE}\e[0m"
+echo -e "\e[35m\e[1mTOKEN length = ${#TOKEN}\e[0m"
 
-echo -e "\e[1mgh api -H \"Accept: application/vnd.github+json\" -H \"X-GitHub-Api-Version: 2022-11-28\" \"/orgs/${ORGANIZATION}/packages?package_type=${PACKAGE_TYPE}&per_page=100&page=1\" | jq -rc '.[].name'\e[0m"
-output=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/orgs/${ORGANIZATION}/packages?package_type=${PACKAGE_TYPE}&per_page=100&page=1")
-echo -e "return value: $?"
-echo -e "output: ${output}"
-packages=$(jq -rc '.[].name' <<< "${output}")
-echo -e "packages:\n${packages}"
-echo -e "\e[1mcurl -L -H \"Accept: application/vnd.github+json\" -H \"Authorization: Bearer \${GH_TOKEN}\" -H \"X-GitHub-Api-Version: 2022-11-28\" \"https://api.github.com/orgs/${ORGANIZATION}/packages?package_type=${PACKAGE_TYPE}&per_page=100&page=1\" | jq -rc '.[].name'\e[0m"
-output=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GH_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/orgs/${ORGANIZATION}/packages?package_type=${PACKAGE_TYPE}&per_page=100&page=1")
-echo -e "return value: $?"
-echo -e "output: ${output}"
-packages=$(jq -rc '.[].name' <<< "${output}")
-echo -e "packages:\n${packages}"
+echo "::group::curl -L -H \"Accept: application/vnd.github+json\" -H \"Authorization: Bearer \${TOKEN}\" -H \"X-GitHub-Api-Version: 2022-11-28\" \"https://api.github.com/orgs/${ORGANIZATION}/packages?package_type=${PACKAGE_TYPE}&per_page=100&page=1\""
+echo -e "\e[1mcurl -L -H \"Accept: application/vnd.github+json\" -H \"Authorization: Bearer \${TOKEN}\" -H \"X-GitHub-Api-Version: 2022-11-28\" \"https://api.github.com/orgs/${ORGANIZATION}/packages?package_type=${PACKAGE_TYPE}&per_page=100&page=1\""
+output=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/orgs/${ORGANIZATION}/packages?package_type=${PACKAGE_TYPE}&per_page=100&page=1")
+echo -e "${output}" | jq
+echo "::endgroup::"
 
+echo -e "\e[1mjq -r '.[].name'\e[0m"
+packages=$(jq -r '.[].name' <<< "${output}")
 echo -e "\e[35m\e[1mpackages:\e[0m"
 for pkg in ${packages}
 do
-echo -e "\e[35m\e[1m  ${pkg}\e[0m"
-done
-for pkg in ${packages}
-do
-packages_list="${packages_list:+$packages_list, }'${pkg}'"
+    echo -e "\e[35m\e[1m  ${pkg}\e[0m"
+    packages_list="${packages_list:+$packages_list, }'${pkg}'"
 done
 packages_list="[${packages_list}]"
