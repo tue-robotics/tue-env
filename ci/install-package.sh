7135f85b8725a2bf734d3a5aae6b7fb570908d74
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2024-02-28 10:29:14 +0100

    (CI) try bare ssh connection to github

--- ci/install-package.sh
+++ ci/install-package.sh
@@ -208,6 +208,9 @@ then
 
     echo -e "\e[35;1mActive SSH keys:\e[0m"
     docker exec -t tue-env bash -c "ssh-add -l 2>/dev/null" | awk '{print $2}'
+
+    echo -e "\e[35;1mssh -Tv git@ssh.github.com:\e[0m"
+    docker exec -t tue-env bash -c "ssh -Tv git@ssh.github.com"
 fi
 
 # Use docker environment variables in all exec commands instead of script variables
