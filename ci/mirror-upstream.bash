c8c706ac751b4d3b07c662857b82bc324261d29c
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2024-03-10 20:39:18 +0100

    (CI) fix echo styling

--- ci/mirror-upstream.bash
+++ ci/mirror-upstream.bash
@@ -8,7 +8,7 @@ set -o errexit
 # Execute script only in a CI environment
 if [ "$CI" != "true" ]
 then
-    echo -e "\e[35;1m Error!\e[0m Trying to execute a CI script in a non-CI environment. Exiting script."
+    echo -e "\e[35;1mError!\e[0m Trying to execute a CI script in a non-CI environment. Exiting script."
     exit 1
 fi
 
